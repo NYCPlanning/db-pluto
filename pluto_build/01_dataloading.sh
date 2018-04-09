d037787a14a4d23046f856deddb02ce4744e07a1
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-04-09 15:35:12 -0400

    inital steps of build lodaing data, and filling in bbl and zoning information
    Former-commit-id: cbdb304d91b7de8b78413298cfeeecd43132023d

---
+++ pluto_build/01_dataloading.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+################################################################################################
+### OBTAINING DATA
+################################################################################################
+### NOTE: This script requires you to setup the DATABASE_URL environment variable.
+### Directions are in the README.md.
+
+## Load all datasets from sources using the civic data loader
+## https://github.com/NYCPlanning/data-loading-scripts
+
+cd '/prod/data-loading-scripts'
+
+## Open_datasets - PULLING FROM OPEN DATA
+echo 'Loading open source datasets...'
+node loader.js install dcp_zoning_taxlot
+node loader.js install lpc_historic_districts
+node loader.js install lpc_landmarks
+
+## Other_datasets - PULLING FROM PLUTO GitHub repo
+echo 'Loading datasets from PLUTO GitHub repo...'
+node loader.js install pluto_input_allocated
+node loader.js install pluto_input_geocodes
+node loader.js install pluto_input_tc234
+
+
