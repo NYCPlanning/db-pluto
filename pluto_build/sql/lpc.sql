2a10e4244f89fc15480f7e9ca56cc181cd40f97e
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-04-20 18:01:47 -0400

    changing so that it seperates vlaues by ;

--- pluto_build/sql/lpc.sql
+++ pluto_build/sql/lpc.sql
@@ -2,7 +2,7 @@
 -- concatenates historic districts seperated by a comma for lots within more than one Historic District 
 UPDATE pluto a
 SET histdist = c.hist_dist_list
-FROM (SELECT b.bbl, string_agg(b.hist_dist, ', ') AS hist_dist_list
+FROM (SELECT b.bbl, string_agg(b.hist_dist, '; ') AS hist_dist_list
 		FROM  (
 			SELECT DISTINCT bbl, hist_dist 
 			FROM lpc_historic_districts) AS b
@@ -14,7 +14,7 @@ WHERE a.borocode||lpad(a.block, 5, '0')||lpad(a.lot, 4, '0') = c.bbl;
 -- concatenates landmark names seperated by a comma for lots with more than one landmark
 UPDATE pluto a
 SET landmark = c.lm_name_list
-FROM (SELECT b.bbl, string_agg(b.lm_name, ', ') AS lm_name_list
+FROM (SELECT b.bbl, string_agg(b.lm_name, '; ') AS lm_name_list
 		FROM (
 			SELECT DISTINCT bbl, lm_name 
 			FROM lpc_landmarks) AS b
