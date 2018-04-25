f0ac79c85723e0971346e34d35ca568d80666679
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-04-25 10:37:02 -0400

    changing script to insert one record and that its the first in alphabetical order
    Former-commit-id: 487ec1ec2ceabc1f437e23bb460de6a22f2621a2

--- pluto_build/sql/lpc.sql
+++ pluto_build/sql/lpc.sql
@@ -1,22 +1,59 @@
+## INPUTS ALL RECORDS SEPERATED BY A ; ##
+-- -- if the lot is in a historical district add in the name of the Historic District from lpc_historic_districts
+-- -- concatenates historic districts seperated by a semi colon for lots within more than one Historic District 
+-- UPDATE pluto a
+-- SET histdist = c.hist_dist_list
+-- FROM (SELECT b.bbl, string_agg(b.hist_dist, '; ') AS hist_dist_list
+-- 		FROM  (
+-- 			SELECT DISTINCT bbl, hist_dist 
+-- 			FROM lpc_historic_districts
+-- 			ORDER BY hist_dist) AS b
+-- 	WHERE hist_dist <> '0'
+-- 	GROUP BY b.bbl) c
+-- WHERE a.borocode||lpad(a.block, 5, '0')||lpad(a.lot, 4, '0') = c.bbl;
+
+-- -- if the lot contains a landmark add in the name of the landmark from lpc_landmarks
+-- -- concatenates landmark names seperated by a semi colon for lots with more than one landmark
+-- UPDATE pluto a
+-- SET landmark = c.lm_name_list
+-- FROM (SELECT b.bbl, string_agg(b.lm_name, '; ') AS lm_name_list
+-- 		FROM (
+-- 			SELECT DISTINCT bbl, lm_name 
+-- 			FROM lpc_landmarks
+-- 			ORDER BY lm_name) AS b
+-- 		GROUP BY b.bbl) c
+-- WHERE a.borocode||lpad(a.block, 5, '0')||lpad(a.lot, 4, '0') = c.bbl;
+
+## INPUTS FIRST ALPHABETICAL RECORD ##
 -- if the lot is in a historical district add in the name of the Historic District from lpc_historic_districts
--- concatenates historic districts seperated by a comma for lots within more than one Historic District 
+-- the first alphabetical historical district is appended
+WITH histdistricts AS (
+	SELECT bbl, hist_dist 
+	FROM (
+		SELECT bbl,hist_dist, ROW_NUMBER()
+    	OVER (PARTITION BY bbl
+      	ORDER BY hist_dist) AS row_number
+  		FROM lpc_historic_districts
+  		WHERE hist_dist <> '0') x
+	WHERE x.row_number = 1)
+
 UPDATE pluto a
-SET histdist = c.hist_dist_list
-FROM (SELECT b.bbl, string_agg(b.hist_dist, '; ') AS hist_dist_list
-		FROM  (
-			SELECT DISTINCT bbl, hist_dist 
-			FROM lpc_historic_districts) AS b
-	WHERE hist_dist <> '0'
-	GROUP BY b.bbl) c
-WHERE a.borocode||lpad(a.block, 5, '0')||lpad(a.lot, 4, '0') = c.bbl;
+SET histdist = histdistricts.hist_dist
+FROM histdistricts
+WHERE a.borocode||lpad(a.block, 5, '0')||lpad(a.lot, 4, '0') = histdistricts.bbl;
 
 -- if the lot contains a landmark add in the name of the landmark from lpc_landmarks
--- concatenates landmark names seperated by a comma for lots with more than one landmark
+-- the first alphabetical landmark is appended
+WITH landmarks AS (
+	SELECT bbl, lm_name
+	FROM (
+		SELECT bbl,lm_name, ROW_NUMBER()
+    	OVER (PARTITION BY bbl
+      	ORDER BY lm_name) AS row_number
+  		FROM lpc_landmarks) x
+	WHERE x.row_number = 1)
+
 UPDATE pluto a
-SET landmark = c.lm_name_list
-FROM (SELECT b.bbl, string_agg(b.lm_name, '; ') AS lm_name_list
-		FROM (
-			SELECT DISTINCT bbl, lm_name 
-			FROM lpc_landmarks) AS b
-		GROUP BY b.bbl) c
-WHERE a.borocode||lpad(a.block, 5, '0')||lpad(a.lot, 4, '0') = c.bbl;
+SET landmark = landmarks.lm_name
+FROM landmarks
+WHERE a.borocode||lpad(a.block, 5, '0')||lpad(a.lot, 4, '0') = landmarks.bbl;
