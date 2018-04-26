4f8fa3829a0bd1c2f914f499cd2e1362d71b3c87
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-04-26 17:55:11 -0400

    adding processes to build PLUTO to shell scripts

---
+++ pluto_build/03_build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# make sure we are at the top of the git directory
+REPOLOC="$(git rev-parse --show-toplevel)"
+cd $REPOLOC
+
+# load config
+DBNAME=$(cat $REPOLOC/pluto.config.json | jq -r '.DBNAME')
+DBUSER=$(cat $REPOLOC/pluto.config.json | jq -r '.DBUSER')
+
+start=$(date +'%T')
+echo "Starting to build PLUTO"
+
+# create the table
+echo 'Creating base PLUTO table'
+psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/create.sql
+psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/bbl.sql
+
+# populate RPAD data
+echo 'Adding on RPAD data attributes'
+psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/allocated.sql
+psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/geocodes.sql
+
+# populate other fields from misc sources
+echo 'Adding on data attributes from other sources'
+psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/landuse.sql
+psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/lpc.sql
+psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/zoning.sql
+
+# add on CAMA data attributes
+echo 'Adding on CAMA data attributes'
+psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/cama_bsmttype.sql
+psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/cama_lottype.sql
+psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/cama_proxcode.sql
+psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/cama_bldgarea.sql
