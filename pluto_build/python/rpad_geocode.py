99612ab4a337823d42a015ba47fafbc79cb228ec
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-07-11 14:46:52 -0400

    starting python script to call geoclient API

--- pluto_build/python/rpad_geocode.py
+++ pluto_build/python/rpad_geocode.py
@@ -88,7 +88,7 @@ for i in range(len(rpad)):
     if (locs['giHighHouseNumber'][i] != 'none'):
         upd = "UPDATE pluto_rpad_geo a SET giHighHouseNumber = " + str(locs['giHighHouseNumber'][i]) + ", giStreetCode = " + str(locs['giStreetCode'][i]) +" WHERE boro = '" + rpad['boro'][i] + "' AND tb = '" + rpad['tb'][i] + "' AND tl = '" + rpad['tl'][i] + "' ;"
     if (locs['rpadBldgClass'][i] != 'none'):
-        upd = "UPDATE pluto_rpad_geo a SET rpadBldgClass = " + str(locs['rpadBldgClass'][i]) + " WHERE boro = '" + rpad['boro'][i] + "' AND tb = '" + rpad['tb'][i] + "' AND tl = '" + rpad['tl'][i] + "' ;"
+        upd = "UPDATE pluto_rpad_geo a SET rpadBldgClass = '" + str(locs['rpadBldgClass'][i]) + "' WHERE boro = '" + rpad['boro'][i] + "' AND tb = '" + rpad['tb'][i] + "' AND tl = '" + rpad['tl'][i] + "' ;"
     elif (locs['lat'][i] == 'none') & (locs['lon'][i] == 'none'):
         upd = "UPDATE pluto_rpad_geo a SET geom = NULL;"
     engine.execute(upd)
