0beb517c7d8bfe923b112d928492527e3808f46b
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-07-16 11:25:09 -0400

    creating geocoding by address returned by first gecoding by bbl
    Former-commit-id: 0a42dbb2c9fbccd605eee4a7019603597f32c252

---
+++ pluto_build/sql/create_rpad_geo.sql
@@ -0,0 +1,38 @@
+-- create RPAD table that is run through Geoclient
+DROP TABLE IF EXISTS pluto_rpad_geo;
+CREATE TABLE pluto_rpad_geo AS (
+SELECT a.*,
+	(CASE
+		WHEN a.boro = '1' THEN 'Manhattan'
+		WHEN a.boro = '2' THEN 'Bronx'
+		WHEN a.boro = '3' THEN 'Brooklyn'
+		WHEN a.boro = '4' THEN 'Queens'
+		WHEN a.boro = '5' THEN 'Staten Island'
+		ELSE NULL
+	END) AS borough
+FROM pluto_rpad a
+LIMIT 100
+);
+
+ALTER TABLE pluto_rpad_geo
+ADD COLUMN bbl text,
+ADD COLUMN billingbbl text,
+ADD COLUMN giHighHouseNumber text,
+ADD COLUMN giStreetCode text,
+ADD COLUMN rpadBldgClass text,
+ADD COLUMN numBldgs text,
+ADD COLUMN cd text,
+ADD COLUMN ct2010 text,
+ADD COLUMN cb2010 text,
+ADD COLUMN schooldist text,
+ADD COLUMN council text,
+ADD COLUMN zipcode text,
+ADD COLUMN firecomp text,
+ADD COLUMN policeprct text,
+ADD COLUMN healthcenterdistrict text,
+ADD COLUMN healtharea text,	
+ADD COLUMN sanitboro text,
+ADD COLUMN sanitdistrict text,
+ADD COLUMN sanitsub text,
+ADD COLUMN address text,
+ADD COLUMN geom geometry;
