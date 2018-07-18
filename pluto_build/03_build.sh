234f147767d96d93635c21e424aa1c1a8ba3bdf1
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-07-18 19:39:38 -0400

    adding notes on how to create base DTM

--- pluto_build/03_build.sh
+++ pluto_build/03_build.sh
@@ -19,7 +19,7 @@ python $REPOLOC/pluto_build/python/rpad_geocode_bbl.py
 python $REPOLOC/pluto_build/python/rpad_geocode_address.py
 source deactivate
 
- node ./3_geoprocessing/geoclient_bbl.js
+ node pluto_build/python/geoclient_bbl.js
 
 
 psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/zerovacantlots.sql
