ac717be541c64cd8f9ce5b622f1c14f11f1d38a5
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-07-19 16:35:01 -0400

    adding -- create RPAD table that is run through Geoclient DROP TABLE IF EXISTS pluto_rpad_geo; CREATE TABLE pluto_rpad_geo AS ( SELECT a.*, (CASE WHEN a.boro = '1' THEN 'Manhattan' WHEN a.boro = '2' THEN 'Bronx' WHEN a.boro = '3' THEN 'Brooklyn' WHEN a.boro = '4' THEN 'Queens' WHEN a.boro = '5' THEN 'Staten Island' ELSE NULL END) AS borough FROM pluto_rpad a LIMIT 100 );
    ALTER TABLE pluto_rpad_geo
    ADD COLUMN bbl text,
    ADD COLUMN billingbbl text,
    ADD COLUMN giHighHouseNumber1 text,
    ADD COLUMN giStreetName1 text,
    ADD COLUMN buildingIdentificationNumber text,
    ADD COLUMN numberOfExistingStructuresOnLot text,
    ADD COLUMN cd text,
    ADD COLUMN ct2010 text,
    ADD COLUMN cb2010 text,
    ADD COLUMN schooldist text,
    ADD COLUMN council text,
    ADD COLUMN zipcode text,
    ADD COLUMN firecomp text,
    ADD COLUMN policeprct text,
    ADD COLUMN healthcenterdistrict text,
    ADD COLUMN healtharea text,
    ADD COLUMN sanitboro text,
    ADD COLUMN sanitdistrict text,
    ADD COLUMN sanitsub text,
    ADD COLUMN address text,

--- pluto_build/python/rpad_geocode_bbl.py
+++ pluto_build/python/rpad_geocode_bbl.py
@@ -52,6 +52,10 @@ def get_loc(borough, block, lot):
     except:
         numberOfExistingStructuresOnLot = 'none'
     try:
+        buildingIdentificationNumber = geo['buildingIdentificationNumber']
+    except:
+        buildingIdentificationNumber = 'none'
+    try:
         latitudeInternalLabel = geo['latitudeInternalLabel']
     except:
         latitudeInternalLabel = 'none'
@@ -63,8 +67,7 @@ def get_loc(borough, block, lot):
                         'giHighHouseNumber1' : [giHighHouseNumber1],
                         'giStreetName1' : [giStreetName1],
                         'numberOfExistingStructuresOnLot' : [numberOfExistingStructuresOnLot],
-                        'latitudeInternalLabel' : [latitudeInternalLabel],
-                        'longitudeInternalLabel' : [longitudeInternalLabel]                       
+                        'buildingIdentificationNumber' : [buildingIdentificationNumber]                     
                         })
     return(loc)
 
@@ -81,7 +84,7 @@ locs.reset_index(inplace = True)
 
 for i in range(len(rpad)):
     if (locs['billingbbl'][i] != 'none'):
-        upd = "UPDATE pluto_rpad_geo a SET billingbbl = '" + str(locs['billingbbl'][i]) + "', giHighHouseNumber1 = '" + str(locs['giHighHouseNumber1'][i]) + "', giStreetName1 = '" + str(locs['giStreetName1'][i]) + "', numberOfExistingStructuresOnLot = " + str(locs['numberOfExistingStructuresOnLot'][i]) + "  WHERE borough = '" + rpad['borough'][i] + "' AND tb = '" + rpad['tb'][i] + "' AND tl = '" + rpad['tl'][i] + "' ;"
+        upd = "UPDATE pluto_rpad_geo a SET billingbbl = '" + str(locs['billingbbl'][i]) + "', giHighHouseNumber1 = '" + str(locs['giHighHouseNumber1'][i]) + "', giStreetName1 = '" + str(locs['giStreetName1'][i]) + "', buildingIdentificationNumber = '" + str(locs['buildingIdentificationNumber'][i]) + "', numberOfExistingStructuresOnLot = " + str(locs['numberOfExistingStructuresOnLot'][i]) + "  WHERE borough = '" + rpad['borough'][i] + "' AND tb = '" + rpad['tb'][i] + "' AND tl = '" + rpad['tl'][i] + "' ;"
     elif (locs['giHighHouseNumber1'][i] == 'none'):
         upd = "UPDATE pluto_rpad_geo a SET geom = ST_SetSRID(ST_MakePoint(" + str(locs['longitudeInternalLabel'][i]) + ", " + str(locs['latitudeInternalLabel'][i]) + "), 4326) WHERE borough = '" + rpad['borough'][i] + "' AND tb = '" + rpad['tb'][i] + "' AND tl = '" + rpad['tl'][i] + "' ;"
     engine.execute(upd)
