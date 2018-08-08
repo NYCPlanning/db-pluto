bb1f3266b7572f557c0534a921b6aa25f1bc9f09
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-08-08 18:00:24 -0400

    adding in fields that are summed by billing bbl

--- pluto_build/sql/create_allocated.sql
+++ pluto_build/sql/create_allocated.sql
@@ -66,14 +66,11 @@ AND b.condo_number <> '0';
 -- populate the fields that where values are aggregated
 UPDATE pluto_allocated a
 SET unitsres = SUM(coop_apts::integer),
-	unitstotal = SUM(units::integer)
+	unitstotal = SUM(units::integer),
+	assessland = SUM(curavl_act::double precision),
+	assesstot = SUM(curavt_act::double precision),
+	exemptland =
+	exempttot = 
 FROM pluto_rpad_geo b
 WHERE a.bbl=b.primebbl
 GROUP BY primebbl;
- 
-
-
-psudo code
-get doe preffered street name, use prime house number, and get the number of buildings on lot to from geocoding it pick up number of structures, which gets inserted into number of building field to replace RPAD value
-
-how do you get the right owner?
