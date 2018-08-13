4e9d41f0948648a06d521cc886e58dc41a7410de
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-08-13 14:14:28 -0400

    finalizing what datasets are needed to build PLUTO / MapPLUTO
    Former-commit-id: 4b1e136bd9155b5ab472b2baa63adb52e23c309e

--- pluto_build/01_dataloading.sh
+++ pluto_build/01_dataloading.sh
@@ -13,38 +13,23 @@ cd '/prod/data-loading-scripts'
 
 ## Open_datasets - PULLING FROM OPEN DATA
 echo 'Loading open source datasets...'
-node loader.js install dcp_zoning_taxlot
+node loader.js install dcp_edesignation
+node loader.js install dcas_facilities_colp
+node loader.js install dcas_ipis
 node loader.js install lpc_historic_districts
 node loader.js install lpc_landmarks
-node loader.js install dcas_ipis
-node loader.js install dcp_edesignation
-node loader.js install dof_dtm
-node loader.js install doitt_buildingfootprints
-node loader.js install dcas_facilities_colp
+## Once edm version is published: node loader.js install dcp_zoning_taxlot
 
 ## Other_datasets - PULLING FROM FTP or PLUTO GitHub repo
 echo 'Loading datasets from PLUTO GitHub repo...'
-node loader.js install pluto_input_allocated
-node loader.js install pluto_input_geocodes
-node loader.js install pluto_input_tc234
-node loader.js install pluto_input_landuse_bldgclass
+node loader.js install dcp_zoning_maxfar
 node loader.js install pluto_input_bsmtcode
+node loader.js install pluto_input_landuse_bldgclass
+
+echo 'Loading datasets from FTP...'
+node loader.js install pluto_rpad
 node loader.js install pluto_input_cama_dof
-node loader.js install dcp_zoning_comm
-node loader.js install dcp_zoning_mnf
-node loader.js install dcp_zoning_res1to5
-node loader.js install dcp_zoning_res6to10
+node loader.js install dof_dtm
+node loader.js install dof_shoreline
 node loader.js install dof_condo
 node loader.js install dof_condo_units
-
-DBSOURCE=$(cat $REPOLOC/cpdb.config.json | jq -r '.DBSOURCE')
-
-echo $DBSOURCE
-
-# create helper views
-#default to run on scraped data from Capital Commitment Plan
-if [ $DBSOURCE = 'fisa_capitalcommitments' ]; then
-    psql -U $DBUSER -d $DBNAME -f $REPOLOC/capitalprojects_build/sql/projects_fisa.sql
-else
-    psql -U $DBUSER -d $DBNAME -f $REPOLOC/capitalprojects_build/sql/projects.sql
-fi
