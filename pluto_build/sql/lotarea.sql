4de591a0e3d6d76123967b32d05cb25775f1b0f2
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-08-15 15:56:00 -0400

    correcting errors in code so that code runs on full RPAD data
    Former-commit-id: 44e5162dc0dfb3e6a1ba9bbea9138f55b8895149

--- pluto_build/sql/lotarea.sql
+++ pluto_build/sql/lotarea.sql
@@ -2,23 +2,25 @@
 -- if lot area = 0 or is NULL and if lot is not irregular and 
 -- and if lot depth and lot front have valid values then 
 -- multiply lot front x lot depth to get lot area
-UPDATE pluto_rpad_geo
-SET land_area = lfft::double precision * ldft::double precision
+UPDATE pluto_rpad_geo b
+SET land_area = (a.lfft::double precision * a.ldft::double precision)
 FROM pluto_rpad_geo a
 WHERE (a.land_area IS NULL OR a.land_area = '0') 
 	AND a.irreg <> 'I'
 	AND a.lfft ~ '[0-9]' 
 	AND a.ldft ~ '[0-9]'
 	AND a.lfft::double precision > 0 
-	AND a.ldft::double precision > 0;
+	AND a.ldft::double precision > 0
+	AND a.boro||a.tb||a.tl=b.boro||b.tb||b.tl;
 
 -- if lot area = 0 or is NULL 
 -- and lot depth =  ACRE 
 -- then lot area = lot front x 43560 + .5
-UPDATE pluto_rpad_geo
-SET land_area = lfft::double precision * 43560 + 0.5
+UPDATE pluto_rpad_geo b
+SET land_area = (a.lfft::double precision * 43560 + 0.5)
 FROM pluto_rpad_geo a
 WHERE (a.land_area IS NULL OR a.land_area = '0')
-	AND upper(ldft) LIKE '%ACRE%'
+	AND upper(a.ldft) LIKE '%ACRE%'
 	AND a.lfft ~ '[0-9]'
-	AND a.lfft::double precision > 0;
+	AND a.lfft::double precision > 0
+		AND a.boro||a.tb||a.tl=b.boro||b.tb||b.tl;
--- pluto_build/sql/zerovacantlots.sql
+++ pluto_build/sql/zerovacantlots.sql
@@ -1,7 +1,6 @@
 -- Zeroing out values for vacant land
 -- If vacant building and AV land = AV total 
 -- then zero out building frontage, building debth, num stories, and number of buildings
-SELECT COUNT(*) 
 UPDATE pluto_rpad_geo a
 SET bfft = '0',
 	bdft = '0',
