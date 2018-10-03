2e1f08adffe56652680f7c7d75334aca015d3cc7
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-10-03 19:47:09 -0400

    fixing bugs

--- pluto_build/sql/versions.sql
+++ pluto_build/sql/versions.sql
@@ -1,9 +1,10 @@
 -- manually set the version of PLUTO and the versions of the source datasets
 UPDATE pluto 
-SET version = 'b18v1.1',
-	rpaddate = '08/04/2018',
-	dcasdate = '08/13/2018',
-	zoningdate = '06/28/18',
-	landmkdate = '08/13/2018',
-	basempdate = '08/13/2018',
-	edesigdate = '08/13/2018';
+SET version = 'b18v2',
+	rpaddate = '09/28/2018',
+	dcasdate = '09/24/2018',
+	zoningdate = '09/21/18',
+	landmkdate = '09/24/2018',
+	basempdate = '09/24/2018',
+	edesigdate = '09/24/2018',
+	tract2010 = ct2010;
