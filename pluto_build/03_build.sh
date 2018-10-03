dd729387eeb9ee3787da9c0fdba9d2fa2171dd88
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-10-03 10:40:45 -0400

    commenting out code that won't run for this version of PLUTO
    Former-commit-id: 7fcd6891d63a2b2efac38d0bbdbf4d68a671f9f2

--- pluto_build/03_build.sh
+++ pluto_build/03_build.sh
@@ -13,38 +13,38 @@ DBUSER=$(cat $REPOLOC/pluto.config.json | jq -r '.DBUSER')
 start=$(date +'%T')
 echo "Starting to build PLUTO"
 
-psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/create_rpad_geo.sql
+# psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/create_rpad_geo.sql
 
-echo 'Geocoding RPAD...'
-source activate py2
-python $REPOLOC/pluto_build/python/rpad_geocode_address.py
-python $REPOLOC/pluto_build/python/rpad_geocode_addressymo.py
-psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/geocode_nones.sql
-# getting address if address in RPAD did not geocode
-python $REPOLOC/pluto_build/python/rpad_geocode_bbl.py
-psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/geocode_billbbl.sql
-python $REPOLOC/pluto_build/python/rpad_geocode_billbbl.py
-psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/geocode_nones.sql
-python $REPOLOC/pluto_build/python/rpad_geocode_bin.py
-psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/geocode_nones.sql
-# using GeoClient address to get spatial attributes
-python $REPOLOC/pluto_build/python/rpad_geocode_addresspt2.py
-source deactivate
+# echo 'Geocoding RPAD...'
+# source activate py2
+# python $REPOLOC/pluto_build/python/rpad_geocode_address.py
+# python $REPOLOC/pluto_build/python/rpad_geocode_addressymo.py
+# psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/geocode_nones.sql
+# # getting address if address in RPAD did not geocode
+# python $REPOLOC/pluto_build/python/rpad_geocode_bbl.py
+# psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/geocode_billbbl.sql
+# python $REPOLOC/pluto_build/python/rpad_geocode_billbbl.py
+# psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/geocode_nones.sql
+# python $REPOLOC/pluto_build/python/rpad_geocode_bin.py
+# psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/geocode_nones.sql
+# # using GeoClient address to get spatial attributes
+# python $REPOLOC/pluto_build/python/rpad_geocode_addresspt2.py
+# source deactivate
 
-psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/geocode_nones.sql
+# psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/geocode_nones.sql
 
-echo 'Reporting records that did not get geocoded...'
-psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/geocode_notgeocoded.sql
+# echo 'Reporting records that did not get geocoded...'
+# psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/geocode_notgeocoded.sql
 
-echo 'Making DCP edits to RPAD...'
-psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/zerovacantlots.sql
-psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/lotarea.sql
-psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/primebbl.sql
-python $REPOLOC/pluto_build/python/app_date.py
+# echo 'Making DCP edits to RPAD...'
+# psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/zerovacantlots.sql
+# psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/lotarea.sql
+# psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/primebbl.sql
+# python $REPOLOC/pluto_build/python/app_date.py
 
 
-echo 'Creating table that aggregates condo data and is used to build PLUTO...'
-psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/create_allocated.sql
+# echo 'Creating table that aggregates condo data and is used to build PLUTO...'
+# psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/create_allocated.sql
 psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/yearbuilt.sql
 
 # create the table
@@ -84,11 +84,11 @@ psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/address.sql
 psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/sanitboro.sql
 psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/clean.sql
 
-echo 'Create base DTM'
-psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/dedupecondotable.sql
-psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/dtmmergepolygons.sql
-psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/plutogeoms.sql
-psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/shorelineclip.sql
+# echo 'Create base DTM'
+# psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/dedupecondotable.sql
+# psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/dtmmergepolygons.sql
+# psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/plutogeoms.sql
+# psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/shorelineclip.sql
 
 
 ##psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/colp.sql
