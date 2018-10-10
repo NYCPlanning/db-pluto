dc8f599d381497c9ece01e1ff6c1de2baa455eb5
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-10-10 18:07:00 -0400

    adding logic that if there is 2 or more of any zoning feature that a lot is considered split
    Former-commit-id: 6a8ecbdfbd9bfd92219304657193063971961fa2

--- pluto_build/sql/zoning.sql
+++ pluto_build/sql/zoning.sql
@@ -19,7 +19,24 @@ WHERE a.bbl = b.bbl;
 -- calculate if tax lot is split by two or more zoning boundary lines and update splitzone
 UPDATE pluto 
 SET splitzone = 'Y'
-WHERE zonedist2 IS NOT NULL OR overlay2 IS NOT NULL OR spdist2 IS NOT NULL;
+WHERE zonedist1 IS NOT NULL
+AND (zonedist2 IS NOT NULL 
+	OR overlay1 IS NOT NULL 
+	OR spdist1 IS NOT NULL);
+
+UPDATE pluto 
+SET splitzone = 'Y'
+WHERE overlay1 IS NOT NULL
+AND (zonedist1 IS NOT NULL 
+	OR overlay2 IS NOT NULL 
+	OR spdist1 IS NOT NULL);
+
+UPDATE pluto 
+SET splitzone = 'Y'
+WHERE spdist1 IS NOT NULL
+AND (zonedist1 IS NOT NULL 
+	OR overlay1 IS NOT NULL 
+	OR spdist2 IS NOT NULL);
 
 UPDATE pluto 
 SET splitzone = 'N'
