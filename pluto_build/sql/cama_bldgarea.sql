c2b30ee54be42ddbaae15483974ecf4434e8b5b9
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-12-27 13:01:17 -0500

    correcting how building area and area source is calculated

--- pluto_build/sql/cama_bldgarea.sql
+++ pluto_build/sql/cama_bldgarea.sql
@@ -23,15 +23,15 @@ SET bldgarea = b.grossarea,
 areasource = '7'
 FROM pluto_input_cama_dof b
 WHERE a.bbl=b.boro||b.block||b.lot
-AND (bldgarea = '0' OR bldgarea IS NULL);
+AND (bldgarea::numeric = 0 OR bldgarea IS NULL);
 
 -- calcualte bldgarea by multiplying bldgfront x bldgdepth
 -- set area source to 5
 UPDATE pluto a
 SET bldgarea = a.bldgfront::numeric*a.bldgdepth::numeric,
 areasource = '5'
-WHERE (bldgarea = '0' OR bldgarea IS NULL)
-AND a.bldgfront <> '0'
+WHERE (a.bldgarea::numeric = 0 OR a.bldgarea IS NULL)
+AND a.bldgfront::numeric <> 0
 AND a.areasource IS NULL;
 
 -- set area source to 4 for vacant lots
@@ -40,11 +40,11 @@ UPDATE pluto a
 SET areasource = '4'
 WHERE areasource IS NULL
 	AND landuse = '11'
-	AND numbldgs = '0'
-	AND bldgarea = '0';
+	AND numbldgs::numeric = 0
+	AND (bldgarea::numeric = 0 OR bldgarea IS NULL);
 
 -- set area source to 0 where building area is not avialble because it's still 0 or null
 UPDATE pluto a
 SET areasource = '0'
 WHERE a.areasource IS NULL
-AND (bldgarea = '0' OR bldgarea IS NULL);
+AND (bldgarea::numeric = 0 OR bldgarea IS NULL);
