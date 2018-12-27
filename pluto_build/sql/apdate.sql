2adf06b9b724a1bd11a7e3c11cbc1eaa002b922b
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-12-27 11:08:24 -0500

    fixing date format
    Former-commit-id: d2abea8219cdc8127910ee96c8e8f3cb2e0864f2

--- pluto_build/sql/apdate.sql
+++ pluto_build/sql/apdate.sql
@@ -1,6 +1,6 @@
--- converting datetime e.g. 93005 --> 1993-01-05
+-- converting datetime from year and day of year e.g. 93005 to 1993-01-05 then MM/DD/YYYY
 UPDATE pluto_rpad_geo
-SET ap_date =
-    CASE WHEN length(ap_date) = 5 THEN to_date(ap_date, 'YYDDD')
+SET ap_datef =
+    CASE WHEN length(ap_date) = 5 THEN to_char(to_date(ap_date, 'YYDDD'), 'MM/DD/YYYY')
     ELSE NULL
-    END
+    END;
