1e836e572e2e390d1b0547d3d3ff38e07b12c9d3
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-12-27 15:01:16 -0500

    correcting issues

--- pluto_build/sql/shorelineclip.sql
+++ pluto_build/sql/shorelineclip.sql
@@ -4,13 +4,13 @@ CREATE TABLE dof_shoreline_union AS
 SELECT ST_Union(geom) as geom
 FROM dof_shoreline;
 
-DROP TABLE IF EXISTS mappluto_clipped;
-CREATE TABLE mappluto_clipped AS (
-	SELECT * FROM pluto
-	WHERE geom IS NOT NULL);
+-- DROP TABLE IF EXISTS mappluto_clipped;
+-- CREATE TABLE mappluto_clipped AS (
+-- 	SELECT * FROM pluto
+-- 	WHERE geom IS NOT NULL);
 
--- clip mappluto to shoreline
-UPDATE mappluto_clipped a
-SET geom = ST_Difference(a.geom, b.geom)
-FROM dof_shoreline b
-WHERE a.geom && b.geom;
+-- -- clip mappluto to shoreline
+-- UPDATE mappluto_clipped a
+-- SET geom = ST_Difference(a.geom, b.geom)
+-- FROM dof_shoreline b
+-- WHERE a.geom && b.geom;
