584b1708578b0bd6b7d1a628549184e9d0746a18
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-12-28 11:24:31 -0500

    adding and removing necessary datasets

--- pluto_build/01_dataloading.sh
+++ pluto_build/01_dataloading.sh
@@ -9,16 +9,22 @@
 ## Load all datasets from sources using the civic data loader
 ## https://github.com/NYCPlanning/data-loading-scripts
 
-cd '/prod/data-loading-scripts'
+# make sure we are at the top of the git directory
+REPOLOC="$(git rev-parse --show-toplevel)"
+cd $REPOLOC
+# go back one level to folder with all repos
+cd ..
+# enter the data-loading-scripts repo
+cd 'data-loading-scripts'
 
 ## Open_datasets - PULLING FROM OPEN DATA
 echo 'Loading open source datasets...'
 node loader.js install dcp_edesignation
-# node loader.js install dcas_facilities_colp
+node loader.js install dcp_zoning_taxlot
+node loader.js install dcas_facilities_colp
 # node loader.js install dcas_ipis
 node loader.js install lpc_historic_districts
 node loader.js install lpc_landmarks
-## Once edm version is published: node loader.js install dcp_zoning_taxlot
 
 ## Other_datasets - PULLING FROM FTP or PLUTO GitHub repo
 echo 'Loading datasets from PLUTO GitHub repo...'
