aed7c0829cd9d94a32463248b307be8781258ad0
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2019-01-15 10:57:21 -0500

    correcting issues
    Former-commit-id: 6b0097e66065fedcc9559c41a451866bdb54e73d

--- pluto_build/sql/landuse.sql
+++ pluto_build/sql/landuse.sql
@@ -4,3 +4,12 @@ UPDATE pluto a
 SET landuse = b.landuse
 FROM pluto_input_landuse_bldgclass b
 WHERE a.bldgclass = b.bldgclass;
+
+-- set area source to 4 for vacant lots
+-- for vacant lots and number of buildings is 0 and building floor area is 0
+UPDATE pluto a
+SET areasource = '4'
+WHERE areasource IS NULL
+	AND landuse = '11'
+	AND numbldgs::numeric = 0
+	AND (bldgarea::numeric = 0 OR bldgarea IS NULL);
