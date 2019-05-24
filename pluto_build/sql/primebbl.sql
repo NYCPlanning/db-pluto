5193c476a8f15dc31d3e5551ea38e1978fde8833
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2019-05-24 09:57:59 -0400

    assigning prime BBL to raw PTS as well because this file is replaceing some CAMA fields

--- pluto_build/sql/primebbl.sql
+++ pluto_build/sql/primebbl.sql
@@ -1,9 +1,21 @@
--- assign the prime BBL to each RPAD record
+-- assign the prime BBL to each PTS collapsed record
 -- assign prime bbl for condo records
 UPDATE pluto_rpad_geo
 SET primebbl = billingbbl
 WHERE billingbbl IS NOT NULL AND billingbbl <> '0000000000';
 -- assign prime bbl for noncondo lots
 UPDATE pluto_rpad_geo
+SET primebbl = boro||tb||tl
+WHERE primebbl IS NULL;
+
+-- assign the prime BBL to each PTS record
+-- assign prime bbl for condo records
+UPDATE dof_pts_propmaster a
+SET primebbl = b.billingbbl
+FROM pluto_input_geocodes b
+WHERE a.boro||a.tb||a.tl=b.borough||lpad(b.block,5,'0')||lpad(b.lot,4,'0')
+AND b.billingbbl IS NOT NULL AND billingbbl <> '0000000000';
+-- assign prime bbl for noncondo lots
+UPDATE dof_pts_propmaster
 SET primebbl = boro||tb||tl
 WHERE primebbl IS NULL;
