9770d7819ebddfa1dbf03cbd7b490ea770727159
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2019-07-23 12:15:54 -0400

    removing trim of leading 0
    Former-commit-id: 1ccf4fdf11c66f8f994c30620e13ca70f86c9d58

--- pluto_build/sql/create_allocated.sql
+++ pluto_build/sql/create_allocated.sql
@@ -55,7 +55,7 @@ SET bldgclass = bldgcl,
 	yearalter2 = yralt2,
 	ownername = owner,
 	irrlotcode = irreg,
-	address = trim(leading '0' FROM housenum_lo)||' '||street_name,
+	address = housenum_lo' '||street_name,
 -- set the number of buildings on a lot
 -- if GeoClient returned a value then we use the value GeoClient returned
 	numbldgs = (CASE 
@@ -85,7 +85,7 @@ SET bldgclass = bldgcl,
 	yearalter2 = yralt2,
 	ownername = owner,
 	irrlotcode = irreg,
-	address = trim(leading '0' FROM housenum_lo)||' '||street_name,
+	address = housenum_lo||' '||street_name,
 -- set the number of buildings on a lot
 -- if GeoClient returned a value then we use the value GeoClient returned
 	numbldgs = (CASE 
