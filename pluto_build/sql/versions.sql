48ba0c52c394135d6f9a7f0d46876e295b076257
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2019-07-26 11:24:31 -0400

    updating dates
    Former-commit-id: 7591f77d3ecac5d00fbde5279832dfbe24df290a

--- pluto_build/sql/versions.sql
+++ pluto_build/sql/versions.sql
@@ -3,7 +3,7 @@ UPDATE pluto
 SET version = '19v1',
 	rpaddate = '07/06/2019',
 	dcasdate = '10/25/2018',
-	zoningdate = '07/08/2019',
+	zoningdate = '07/17/2019',
 	landmkdate = '07/05/2019',
-	basempdate = '07/05/2019',
+	basempdate = '07/12/2019',
 	edesigdate = '05/31/2019';
