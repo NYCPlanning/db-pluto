b6dd169f712d0d6c9173503d1b9450f29016f46a
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2019-08-01 11:06:42 -0400

    fill in missing attributes from supplementary table
    Former-commit-id: b211c926d8edd78470dd3c125d46806c531a6e8e

--- pluto_build/sql/create_allocated.sql
+++ pluto_build/sql/create_allocated.sql
@@ -140,15 +140,11 @@ WHERE a.bbl=b.primebbl;
 
 -- fill in missing attributes from supplementary table (temp solution)
 UPDATE pluto_allocated a
-SET lotarea = a.,
-	numfloors = ,
-	yearbuilt = ,
+SET lotarea = b.landsize,
+	numfloors = b.story,
+	yearbuilt = b.yearbuilt
 FROM pluto_input_condolot_descriptiveattributes b
 WHERE a.bbl = b.parid
 AND a.lotarea::numeric = 0 
 AND a.numfloors::numeric = 0 
-AND a.yearbuilt::numeric = 0 
-
-
-
-
+AND a.yearbuilt::numeric = 0;
