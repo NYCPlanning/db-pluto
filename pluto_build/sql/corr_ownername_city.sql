9b914fefcc865c0e82bbd5900e188adcb57bdd28
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2019-10-11 18:35:29 -0400

    adding logic to just insert new records based off of researched tables

--- pluto_build/sql/corr_ownername_city.sql
+++ pluto_build/sql/corr_ownername_city.sql
@@ -15,10 +15,10 @@ UPDATE pluto_corr_ownername_temp
 SET newownername = replace(newownername,'C N Y', 'NYC');
 
 UPDATE pluto_corr_ownername_temp
-SET newownername = replace(newownername,'DEPT', 'DEPARTMENT');
+SET newownername = replace(newownername,'NY STATE', 'NYS');
 
 UPDATE pluto_corr_ownername_temp
-SET newownername = replace(newownername,'DPT', 'DEPARTMENT');
+SET newownername = replace(newownername,'DEPT', 'DEPARTMENT');
 
 UPDATE pluto_corr_ownername_temp
 SET newownername = replace(newownername,'DPT', 'DEPARTMENT');
@@ -31,11 +31,6 @@ SET newownername = replace(newownername,'DCAS', 'DEPARTMENT OF CITYWIDE ADMINIST
 
 UPDATE pluto_corr_ownername_temp
 SET newownername = replace(newownername,'DDC', 'DEPARTMENT OF DESIGN AND CONSTRUCTION');
-
-UPDATE pluto_corr_ownername_temp
-SET newownername = replace(newownername,'DEP', 'DEPARTMENT OF ENVIRONMENTAL PROTECTION')
-WHERE newownername = 'DEP'
-	OR newownername = 'NYC DEP';
 
 UPDATE pluto_corr_ownername_temp
 SET newownername = replace(newownername,'DHS', 'DEPARTMENT OF HOMELESS SERVICES');
@@ -62,7 +57,7 @@ UPDATE pluto_corr_ownername_temp
 SET newownername = replace(newownername,'HRA', 'HUMAN RESOURCES ADMINISTRATION');
 
 UPDATE pluto_corr_ownername_temp
-SET newownername = replace(newownername,'OCME', 'OFFICE OF CHIEF MEDICAL EXAMINER');
+SET newownername = replace(newownername,'OCME', 'OFFICE OF THE CHIEF MEDICAL EXAMINER');
 
 UPDATE pluto_corr_ownername_temp
 SET newownername = replace(newownername,'LIBRA', 'LIBRARY')
@@ -87,6 +82,10 @@ WHERE newownername LIKE '%CORP';
 UPDATE pluto_corr_ownername_temp
 SET newownername = replace(newownername,'AUTH', 'AUTHORITY')
 WHERE newownername LIKE '%AUTH';
+
+UPDATE pluto_corr_ownername_temp
+SET newownername = replace(newownername,'ACD', 'ADJOURMENTS IN CONTEMPLATION OF DISMISSAL')
+WHERE newownername LIKE '%ACD';
 
 UPDATE pluto_corr_ownername_temp
 SET newownername = replace(newownername,' & ', ' AND ');
@@ -123,8 +122,13 @@ SET newownername = 'NYC DEPARTMENT OF EDUCATION'
 WHERE newownername LIKE 'NYC%EDUCATION%';
 
 UPDATE pluto_corr_ownername_temp
+SET newownername = replace(newownername,'DEP', 'DEPARTMENT OF ENVIRONMENTAL PROTECTION')
+WHERE newownername = 'DEP'
+	OR newownername = 'NYC DEP';
+UPDATE pluto_corr_ownername_temp
 SET newownername = 'NYC DEPARTMENT OF ENVIRONMENTAL PROTECTION'
 WHERE newownername LIKE 'NYC%ENVIRON%PROT%';
+
 
 UPDATE pluto_corr_ownername_temp
 SET newownername = 'NYC POLICE DEPARTMENT'
@@ -143,7 +147,7 @@ SET newownername = 'NYC DEPARTMENT OF SANITATION'
 WHERE newownername LIKE '%SANITATION%';
 
 UPDATE pluto_corr_ownername_temp
-SET newownername = 'PORT AUTHORITY NY AND NJ'
+SET newownername = 'PORT AUTHORITY OF NY AND NJ'
 WHERE newownername LIKE '%PORT%AUTH%';
 
 UPDATE pluto_corr_ownername_temp
@@ -155,7 +159,7 @@ SET newownername = 'MTA - STATEN ISLAND RAILWAY'
 WHERE newownername LIKE '%MTA%-%STATEN%ISLAND%RAILWAY%';
 
 UPDATE pluto_corr_ownername_temp
-SET newownername = 'OFFICE OF CHIEF MEDICAL EXAMINER'
+SET newownername = 'NYC OFFICE OF THE CHIEF MEDICAL EXAMINER'
 WHERE newownername LIKE '%OFFICE%CHIEF%MEDICAL%';
 
 UPDATE pluto_corr_ownername_temp
@@ -179,10 +183,20 @@ WHERE newownername LIKE 'DEPARTMENT%'
 	OR newownername LIKE 'POLICE%'
 	;
 
+UPDATE pluto_corr_ownername_temp
+SET newownername = 'CITY OF NEW YORK'
+WHERE newownername = 'NYC'
+OR newownername = 'THE NYC';
 
-1 | NAME NOT ON FILE
-20 | NOT ON FILE
+UPDATE pluto_corr_ownername_temp
+SET newownername = 'NYC FIRE DEPARTMENT'
+WHERE newownername = 'NYC FIRE DEPARTMENT                     250';
 
+UPDATE pluto_corr_ownername_temp
+SET newownername = 'NYC POLICE DEPARTMENT'
+WHERE newownername = 'NYC POLICE DEPARTMENT                   1';
+
+-- output table and use in logic below
 
 SELECT COUNT(*), newownername FROM pluto_corr_ownername_temp GROUP BY newownername ORDER BY newownername; 
 
@@ -190,7 +204,20 @@ SELECT COUNT(*), newownername FROM pluto_corr_ownername_temp GROUP BY newownerna
 SELECT COUNT(*), ownername, newownername FROM pluto_corr_ownername_temp GROUP BY ownername, newownername ORDER BY newownername; 
 
 
+-- Take the year built from researched lpc_historic_districts table
+-- Insert records into pluto_input_corrections for year built from LPC data
+INSERT INTO pluto_input_corrections
+SELECT DISTINCT a.bbl, 
+	'ownername' as field, 
+	a.ownername as old_value, 
+	b.newownername as new_value
+FROM pluto a, pluto_corr_ownername b
+WHERE a.ownername = b.ownername
+	AND a.bbl NOT IN (SELECT bbl FROM pluto_input_corrections WHERE field = 'ownername');
 
-
-SELECT * FROM pluto_corr_ownername_temp
-WHERE newownername LIKE '%AUTH'
+-- Apply correction to PLUTO
+UPDATE pluto a
+SET ownername = b.newownername,
+	dcpedited = 't'
+FROM pluto_corr_ownername b
+WHERE a.ownername = b.ownername;
