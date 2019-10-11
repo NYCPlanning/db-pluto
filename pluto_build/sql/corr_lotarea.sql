52df71d4a571943e530675f85b0209d1e584fbe4
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2019-10-11 18:35:29 -0400

    adding logic to just insert new records based off of researched tables
    Former-commit-id: 9b914fefcc865c0e82bbd5900e188adcb57bdd28

--- pluto_build/sql/corr_lotarea.sql
+++ pluto_build/sql/corr_lotarea.sql
@@ -1,5 +1,18 @@
 -- Where the lot area is zero but the geometry is not null
 -- compute the lot area from the geometry in sq feet
+
+-- Insert old and new values into tracking table
+INSERT INTO pluto_input_corrections b
+SELECT DISTINCT a.bbl, 
+	'lotarea' as field, 
+	a.lotarea as old_value, 
+	round(ST_Area(ST_Transform(a.geom,2263)))::text as new_value
+FROM pluto a
+WHERE a.lotarea = '0' 
+	AND a.geom IS NOT NULL
+	AND a.bbl NOT IN (SELECT bbl FROM pluto_input_corrections WHERE field = 'lotarea');
+	
+-- Apply correction
 UPDATE pluto
 SET lotarea = round(ST_Area(ST_Transform(geom,2263)))::text,
 	dcpedited = 't'
--- pluto_build/sql/corr_yearbuilt_lpc.sql
+++ pluto_build/sql/corr_yearbuilt_lpc.sql
@@ -1,33 +1,19 @@
--- Take the year built from lpc_historic_districts
--- Insert new records into pluto_input_corrections for year built from LPC data
+-- Take the year built from researched lpc_historic_districts table
+-- Insert records into pluto_input_corrections for year built from LPC data
 INSERT INTO pluto_input_corrections
-WITH lpcyears AS (
-SELECT bbl, 
-	min(yearbuilt::numeric)::text as yearbuilt
-FROM lpc_historic_districts
-WHERE yearbuilt::numeric > 0
-GROUP BY bbl)
 SELECT DISTINCT a.bbl, 
 	'yearbuilt' as field, 
 	a.yearbuilt as old_value, 
-	b.yearbuilt as new_value
+	b.lpcyearbuilt as new_value
 FROM pluto a, lpcyears b
 WHERE a.bbl = b.bbl
-	AND a.yearbuilt<>b.yearbuilt
+	AND a.yearbuilt=b.plutoyearbuilt
 	AND a.bbl NOT IN (SELECT bbl FROM pluto_input_corrections WHERE field = 'yearbuilt');
 
--- Update records in pluto_input_corrections for year built from LPC data
-WITH lpcyears AS (
-SELECT bbl, 
-	min(yearbuilt::numeric)::text as yearbuilt
-FROM lpc_historic_districts
-WHERE yearbuilt::numeric > 0
-GROUP BY bbl)
-
-UPDATE pluto_input_corrections a
-SET new_value = b.yearbuilt
-FROM lpcyears b
+-- Apply correction to PLUTO
+UPDATE pluto a
+SET yearbuilt = b.lpcyearbuilt,
+	dcpedited = 't'
+FROM pluto_corr_lpcyears b
 WHERE a.bbl = b.bbl
-	AND a.field = 'yearbuilt'
-	AND a.new_value<>b.yearbuilt
-	AND b.yearbuilt::numeric > 0;
+	AND a.yearbuilt=b.plutoyearbuilt;
