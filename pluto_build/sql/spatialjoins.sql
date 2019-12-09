cdf079efb1bfb7a30a4e77e739fffd378eb7f51a
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2019-12-09 10:17:56 -0500

    adding zip code to spatial join

--- pluto_build/sql/spatialjoins.sql
+++ pluto_build/sql/spatialjoins.sql
@@ -81,3 +81,10 @@ FROM dsny_frequencies b
 WHERE ST_Within(ST_Transform(ST_SetSRID(ST_MakePoint(a.xcoord::double precision,a.ycoord::double precision),2263), 4326),b.wkb_geometry)
 AND (a.sanitsub IS NULL OR a.sanitsub = ' ')
 AND a.xcoord IS NOT NULL;
+
+UPDATE pluto a
+SET zipcode = b.zipcode
+FROM doitt_zipcodeboundaries b
+WHERE ST_Within(ST_Transform(ST_SetSRID(ST_MakePoint(a.xcoord::double precision,a.ycoord::double precision),2263), 4326),b.wkb_geometry)
+AND a.zipcode IS NULL
+AND a.xcoord IS NOT NULL;
