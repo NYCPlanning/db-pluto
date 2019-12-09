cf30a2135a945031425a4b10aa9a65f72e195336
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2019-12-09 13:53:11 -0500

    adding IS NOT NULL clause

--- pluto_build/sql/latlong.sql
+++ pluto_build/sql/latlong.sql
@@ -1,4 +1,5 @@
 -- populate the lat and long fields
 UPDATE pluto a
 SET latitude = ST_Y(ST_Transform(ST_SetSRID(ST_MakePoint(a.xcoord::double precision,a.ycoord::double precision),2263), 4326)),
-	longitude = ST_X(ST_Transform(ST_SetSRID(ST_MakePoint(a.xcoord::double precision,a.ycoord::double precision),2263), 4326));
+	longitude = ST_X(ST_Transform(ST_SetSRID(ST_MakePoint(a.xcoord::double precision,a.ycoord::double precision),2263), 4326))
+WHERE a.xcoord IS NOT NULL;
