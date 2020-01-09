571608efb01cdf331e443614d86dc532f026bc95
sptkl <caobaiyue@gmail.com>
2020-01-09 02:36:05 +0000

    20v1 update
    Former-commit-id: a83b17ce8dd823bcdb78173ae590177e715584c1

--- pluto_build/04_corrections.sh
+++ pluto_build/04_corrections.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
+#!/bin/sh
+if [ -f .env ]
+then
+  export $(cat .env | sed 's/#.*//g' | xargs)
+fi
+
 psql $BUILD_ENGINE -f sql/corr_create.sql
 
 echo "Applying corrections to PLUTO"
@@ -10,24 +15,39 @@ psql $BUILD_ENGINE -f sql/corr_dropoldrecords.sql
 psql $BUILD_ENGINE -f sql/remove_unitlots.sql
 
 
-docker exec pluto bash -c '
-        TABLE_NAME=19v2_w_corrections
-        echo $TABLE_NAME
-        pg_dump -t pluto --no-owner -U postgres -d postgres | psql $EDM_DATA
-        psql $EDM_DATA -c "DROP INDEX idx_pluto_bbl;";
-        psql $EDM_DATA -c "DROP INDEX pbbl_ix;";
-        psql $EDM_DATA -c "DROP INDEX pluto_gix;";
-        psql $EDM_DATA -c "CREATE SCHEMA IF NOT EXISTS dcp_pluto;";
-        psql $EDM_DATA -c "ALTER TABLE pluto SET SCHEMA dcp_pluto;";
-        psql $EDM_DATA -c "DROP TABLE IF EXISTS dcp_pluto.\"$TABLE_NAME\";";
-        psql $EDM_DATA -c "ALTER TABLE dcp_pluto.pluto RENAME TO \"$TABLE_NAME\";";
-    '
+# docker exec pluto bash -c '
+#         TABLE_NAME=19v2_w_corrections
+#         echo $TABLE_NAME
+#         pg_dump -t pluto --no-owner -U postgres -d postgres | psql $EDM_DATA
+#         psql $EDM_DATA -c "DROP INDEX idx_pluto_bbl;";
+#         psql $EDM_DATA -c "DROP INDEX pbbl_ix;";
+#         psql $EDM_DATA -c "DROP INDEX pluto_gix;";
+#         psql $EDM_DATA -c "CREATE SCHEMA IF NOT EXISTS dcp_pluto;";
+#         psql $EDM_DATA -c "ALTER TABLE pluto SET SCHEMA dcp_pluto;";
+#         psql $EDM_DATA -c "DROP TABLE IF EXISTS dcp_pluto.\"$TABLE_NAME\";";
+#         psql $EDM_DATA -c "ALTER TABLE dcp_pluto.pluto RENAME TO \"$TABLE_NAME\";";
+#     '
 
-echo "Exporting pluto csv and shapefile"
-psql $BUILD_ENGINE  -c "\COPY (SELECT * FROM pluto) TO 'output/pluto_w_corrections.csv' DELIMITER ',' CSV HEADER;"
+# echo "Exporting pluto csv and shapefile"
+psql $BUILD_ENGINE  -c "\COPY (SELECT * FROM pluto) TO 'output/pluto.csv' DELIMITER ',' CSV HEADER;"
 
-rm -f output/pluto_w_corrections.zip
-zip output/pluto_w_corrections.zip output/pluto_w_corrections.csv
-rm -f output/pluto_w_corrections.csv
+rm -f output/pluto.zip
+zip output/pluto.zip output/pluto.csv
+rm -f output/pluto.csv
 
 psql $BUILD_ENGINE  -c "\COPY (SELECT * FROM pluto_corrections) TO 'output/pluto_corrections.csv' DELIMITER ',' CSV HEADER;"
+
+curl -d "{
+    \"src_engine\":\"${BUILD_ENGINE}\",
+    \"dst_engine\": \"${EDM_DATA}\",
+    \"src_schema_name\": \"public\",
+    \"dst_schema_name\": \"dcp_pluto\",
+    \"src_version\": \"pluto\",
+    \"dst_version\": \"${VERSION}\"
+    }"\
+    -H "Content-Type: application/json"\
+    -X POST $GATEWAY/migrate
+
+# psql $EDM_DATA -c "DROP VIEW IF EXISTS dcp_zoningtaxlots.latest"
+# psql $EDM_DATA -c "CREATE VIEW dcp_zoningtaxlots.latest AS 
+#                     (SELECT * FROM dcp_zoningtaxlots.\"${DATE}\")"
--- pluto_build/05_export.sh
+++ pluto_build/05_export.sh
@@ -1,28 +1,20 @@
 #!/bin/sh
-# load config
-DBNAME=postgres
-DBUSER=postgres
-
+if [ -f .env ]
+then
+  export $(cat .env | sed 's/#.*//g' | xargs)
+fi
 # some final processing is done in Esri to create the Esri file formats
 # please go to NYC Planning's Bytes of the Big Apple to download the offical versions of PLUTO and MapPLUTO
 # https://www1.nyc.gov/site/planning/data-maps/open-data.page
 
-echo "Exporting pluto csv and shapefile"
-docker exec pluto psql -U $DBUSER -d $DBNAME  -c "\COPY (SELECT * FROM pluto) TO 'output/pluto.csv' DELIMITER ',' CSV HEADER;"
-
-rm output/pluto.zip
-zip output/pluto.zip output/pluto.csv
-rm -f output/pluto.csv
-
 rm output/mappluto.zip
-docker exec pluto pgsql2shp -u $DBUSER -f output/mappluto $DBNAME "SELECT ST_Transform(geom, 2263) FROM pluto WHERE geom IS NOT NULL"
+pgsql2shp -u $BUILD_USER -P $BUILD_PWD -h $BUILD_HOST -p $BUILD_PORT -f output/mappluto $BUILD_DB "SELECT ST_Transform(geom, 2263) FROM pluto WHERE geom IS NOT NULL"
 zip output/mappluto.zip output/mappluto*
 rm -f output/mappluto.cpg
 rm -f output/mappluto.shp
 rm -f output/mappluto.prj
 rm -f output/mappluto.dbf
 rm -f output/mappluto.shx
-
 
 # ogr2ogr -f "GeoJSON" pluto_build/output/mappluto_clipped.geojson PG:"host=localhost dbname=$DBNAME user=$DBUSER" \
 # -sql "SELECT * FROM mappluto_clipped WHERE geom IS NOT NULL"
