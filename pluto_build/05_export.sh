e090892c482d9e7778b4b7f02ab3f41d0cdd552e
sptkl <caobaiyue@gmail.com>
2020-01-14 21:29:33 +0000

    update

--- pluto_build/05_export.sh
+++ pluto_build/05_export.sh
@@ -7,18 +7,18 @@ fi
 URL="$GATEWAY/upload"
 DATE=$(date "+%Y-%m-%d")
 
-# mappluto
-mkdir -p $(pwd)/output/mappluto && 
-        cd $(pwd)/output/mappluto {
-          pgsql2shp -u $BUILD_USER -P $BUILD_PWD -h $BUILD_HOST -p $BUILD_PORT -f mappluto $BUILD_DB "SELECT ST_Transform(geom, 2263) FROM pluto WHERE geom IS NOT NULL"
-          rm -f mappluto_$VERSION.zip
-          zip mappluto_$VERSION.zip mappluto.*
-          curl -X POST $GATEWAY/upload\
-                -F file=@mappluto_$VERSION.zip\
-                -F key=$DATE/mappluto_$VERSION.zip\
-                -F acl=public-read
-          rm -f mappluto.*
-        cd -;}
+# # mappluto
+# mkdir -p $(pwd)/output/mappluto && 
+#         cd $(pwd)/output/mappluto {
+#           pgsql2shp -u $BUILD_USER -h $BUILD_HOST -p $BUILD_PORT -f mappluto $BUILD_DB "SELECT ST_Transform(geom, 2263) FROM pluto WHERE geom IS NOT NULL"
+#           rm -f mappluto_$VERSION.zip
+#           zip mappluto_$VERSION.zip mappluto.*
+#           curl -X POST $GATEWAY/upload\
+#                 -F file=@mappluto_$VERSION.zip\
+#                 -F key=$DATE/mappluto_$VERSION.zip\
+#                 -F acl=public-read
+#           rm -f mappluto.*
+#         cd -;}
 
 # Pluto
 mkdir -p $(pwd)/output/pluto &&
--- pluto_build/python/future_loading.py
+++ pluto_build/python/future_loading.py
@@ -94,5 +94,5 @@ if __name__ == "__main__":
             'fema_pfirms2015_100yr', 
             'pluto_input_condolot_descriptiveattributes']
 
-    with Pool(processes=cpu_count()) as pool:
-        pool.map(ETL, tables)
+    # with Pool(processes=cpu_count()) as pool:
+    #     pool.map(ETL, tables)
