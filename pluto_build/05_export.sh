06020cdf0c438d0532082df7f8a55a99d3c17a4f
sptkl <caobaiyue@gmail.com>
2020-02-20 04:30:52 +0000

    fixing export
    Former-commit-id: 1732b3958b82d590a157603257dca7afeb604515

--- pluto_build/05_export.sh
+++ pluto_build/05_export.sh
@@ -16,9 +16,9 @@ mkdir -p output
 source ./url_parse.sh $BUILD_ENGINE
 # mappluto
 mkdir -p output/mappluto &&
-  cd (output pgsql2shp -u $BUILD_USER -h $BUILD_HOST -p $BUILD_PORT -f mappluto $BUILD_DB 
+  (cd output pgsql2shp -u $BUILD_USER -h $BUILD_HOST -p $BUILD_PORT -f mappluto $BUILD_DB 
       "SELECT ST_Transform(geom, 2263) FROM pluto WHERE geom IS NOT NULL"
-    cd (mappluto rm -f mappluto_$VERSION.zip
+    (cd mappluto rm -f mappluto_$VERSION.zip
       zip mappluto_$VERSION.zip mappluto.*
       rm -f mappluto.*
       )
@@ -26,7 +26,7 @@ mkdir -p output/mappluto &&
 
 # Pluto
 mkdir -p output/pluto &&
-        cd (output/pluto
+        (cd output/pluto
           rm -f pluto_$VERSION.zip
           psql $BUILD_ENGINE -c "\COPY (SELECT * FROM pluto) TO STDOUT DELIMITER ',' CSV HEADER;" > pluto.csv
           zip pluto_$VERSION.zip pluto.csv
