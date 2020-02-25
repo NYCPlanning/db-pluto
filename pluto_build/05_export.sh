181771400fb10d26caadc06f70228a917f78ed45
SPTKL <caobaiyue@gmail.com>
2020-02-25 16:27:42 -0500

    strict select columns for export
    - avoid exporting unwanted columns e.g. centroid (for some reason it wasn't droped)
    [skip ci]

--- pluto_build/05_export.sh
+++ pluto_build/05_export.sh
@@ -41,7 +41,27 @@ mkdir -p output/mappluto &&
 mkdir -p output/pluto &&
   (cd output/pluto
     rm -f pluto.zip
-    psql $BUILD_ENGINE -c "\COPY (SELECT * FROM pluto) TO STDOUT DELIMITER ',' CSV HEADER;" > pluto.csv
+    psql $BUILD_ENGINE -c "\COPY ( 
+          SELECT borough,block,lot,cd,ct2010,cb2010,schooldist,
+          council,zipcode,firecomp,policeprct,healtharea,
+          sanitboro,sanitsub,address,zonedist1,zonedist2,
+          zonedist3,zonedist4,overlay1,overlay2,spdist1,
+          spdist2,spdist3,ltdheight,splitzone,bldgclass,
+          landuse,easements,ownertype,ownername,lotarea,
+          bldgarea,comarea,resarea,officearea,retailarea,
+          garagearea,strgearea,factryarea,otherarea,
+          areasource,numbldgs,numfloors,unitsres,unitstotal,
+          lotfront,lotdepth,bldgfront,bldgdepth,ext,proxcode,
+          irrlotcode,lottype,bsmtcode,assessland,assesstot,
+          exempttot,yearbuilt,yearalter1,yearalter2,histdist,
+          landmark,builtfar,residfar,commfar,facilfar,borocode,
+          bbl,condono,tract2010,xcoord,ycoord,latitude,longitude,
+          zonemap,zmcode,sanborn,taxmap,edesignum,appbbl,appdate,
+          plutomapid,version,sanitdistrict,healthcenterdistrict,
+          firm07_flag,pfirm15_flag,rpaddate,dcasdate,zoningdate,
+          landmkdate,basempdate,masdate,polidate,
+          edesigdate,geom,dcpedited,notes FROM pluto
+          ) TO STDOUT DELIMITER ',' CSV HEADER;" > pluto.csv
     echo "$VERSION" > version.txt
     echo "$(wc -l pluto.csv)" >> version.txt
     zip pluto.zip *
