71bcc76f88b9dc4736e801a96a9e045bc717ea4a
SPTKL <caobaiyue@gmail.com>
2020-05-20 09:23:04 -0400

    adding numfloors corrections (#154)
    * adding numfloors corrections
    
    * Update corr_numfloors.sql
    
    * Fix typos and remove pluto_input_research
    
    * Remove pluto_input_research from PR by setting to v4improvements version
    
    Co-authored-by: mgraber <molly.graber@me.com>

---
+++ pluto_build/sql/corr_numfloors.sql
@@ -0,0 +1,24 @@
+-- Take the numfloors value from research table
+-- Insert records into pluto_corrections
+INSERT INTO pluto_corrections
+SELECT DISTINCT a.bbl, 
+	'numfloors' as field, 
+	a.numfloors as old_value, 
+	b.new_value as new_value,
+	b.type as type,
+	b.reason as reason,
+	b.version as version
+FROM pluto a, pluto_input_research b
+WHERE a.bbl = b.bbl
+	AND a.numfloors=b.old_value
+	AND b.field = 'numfloors'
+	AND a.bbl NOT IN (SELECT bbl FROM pluto_corrections WHERE field = 'numfloors');
+
+-- Apply correction to PLUTO
+UPDATE pluto a
+SET numfloors = b.new_value,
+	dcpedited = 't'
+FROM pluto_corrections b
+WHERE a.bbl = b.bbl
+	AND b.field = 'numfloors'
+	AND a.numfloors=b.old_value;
