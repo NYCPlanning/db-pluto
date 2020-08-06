beb3e13d0a1ce948893f19a2389132cd027b95ad
sptkl <caobaiyue@gmail.com>
2020-08-06 17:53:15 +0000

    refactor dataloading for data tables that live in github repo, remove pluto and mappluto from dataloading and disable backfill;

--- pluto_build/01_dataloading.sh
+++ pluto_build/01_dataloading.sh
@@ -19,14 +19,76 @@ docker run --rm\
     -e RECIPE_ENGINE=$RECIPE_ENGINE\
     sptkl/cook:latest python3 fastloading.py
 
+ psql $BUILD_ENGINE -c "
+ DROP TABLE IF EXISTS pluto_input_research;
+ CREATE TABLE pluto_input_research (
+    bbl text,
+    field text,
+    old_value text,
+    new_value text,
+    Type text,
+    reason text,
+    version text
+ );
+
+DROP TABLE IF EXISTS pluto_input_landuse_bldgclass;
+CREATE TABLE pluto_input_landuse_bldgclass (
+    bldgclass text,
+    landuse text,
+    landusevalue text
+ );
+
+DROP TABLE IF EXISTS pluto_input_condolot_descriptiveattributes;
+CREATE TABLE pluto_input_condolot_descriptiveattributes (
+    CondNO text,
+    Boro text,
+    PARID text,
+    BC text,
+    TC text,
+    LandSize text,
+    Story text,
+    YearBuilt text
+);
+
+DROP TABLE IF EXISTS pluto_input_condo_bldgclass;
+CREATE TABLE pluto_input_condo_bldgclass (
+    code character varying,
+    description character varying,
+    type character varying,
+    dcpcreated character varying,
+    logic character varying
+);
+
+DROP TABLE IF EXISTS pluto_input_bsmtcode;
+CREATE TABLE pluto_input_bsmtcode (
+    bsmnt_type character varying,
+    bsmntgradient character varying,
+    bsmtcode character varying,
+    bsmnt_typevalue character varying,
+    bsmntgradientvalue character varying,
+    bsmtcodevalue character varying
+);
+
+DROP TABLE IF EXISTS dcp_zoning_maxfar;
+CREATE TABLE dcp_zoning_maxfar (
+    zonedist character varying,
+    contextual character varying,
+    zoningdistricttype character varying,
+    residfar character varying,
+    facilfar character varying,
+    commfar character varying,
+    mnffar character varying,
+    column1 character varying,
+    verified character varying,
+    extra character varying
+)
+"
+
 imports_csv pluto_input_research &
 imports_csv pluto_input_landuse_bldgclass &
-imports_csv pluto_input_corrections &
 imports_csv pluto_input_condolot_descriptiveattributes &
 imports_csv pluto_input_condo_bldgclass &
 imports_csv pluto_input_bsmtcode &
-imports_csv lookup_lottype &
-imports_csv lookup_bldgclass &
 imports_csv dcp_zoning_maxfar
 
 # Create data version table 
--- pluto_build/python/fastloading.py
+++ pluto_build/python/fastloading.py
@@ -13,10 +13,12 @@ def ETL(table):
 if __name__ == "__main__":
     tables = [
         "pluto_corrections",
+        "pluto_input_numbldgs",
         "dcp_edesignation",
         "dcas_facilities_colp",
         "lpc_historic_districts",
         "lpc_landmarks",
+
         # for spatial joins
         "dcp_cdboundaries",
         "dcp_censustracts",
@@ -29,20 +31,24 @@ if __name__ == "__main__":
         "dcp_healthareas",
         "dcp_healthcenters",
         "dsny_frequencies",
-        "dcp_pluto",
-        "dcp_mappluto",
         "dpr_greenthumb",
-        # Other_datasets - PULLING FROM FTP
+
+        # PTS
         "pluto_pts",
         "pluto_input_geocodes",
+        
         # raw CAMA data from DOF
         "pluto_input_cama_dof",
+        
         # raw Digital Tax Map from DOF
         "dof_dtm",
+        
         # raw NYC shoreline file from DOF
         "dof_shoreline",
+        
         # raw DOF condo table
         "dof_condo",
+        
         # DCP zoning datasets~
         "dcp_commercialoverlay",
         "dcp_limitedheight",
@@ -51,6 +57,7 @@ if __name__ == "__main__":
         "dcp_specialpurposesubdistricts",
         "dcp_zoningmapamendments",
         "dcp_zoningmapindex",
+        
         # FEMA 2007 and preliminary 2015 100 year flood zones
         "fema_firms2007_100yr",
         "fema_pfirms2015_100yr"
