0613af046881d465051ee0150d55bdaa4b6a30cb
mgraber <mgraber@planning.nyc.gov>
2021-03-30 12:30:56 +0000

    Switch dataloading from fastloading.py to import from data library
    Former-commit-id: c75d8888ca38b1980be10ea99ed92bc28dac8fed

--- pluto_build/01_dataloading.sh
+++ pluto_build/01_dataloading.sh
@@ -12,10 +12,37 @@ BEGIN
 END \$\$;
 "
 
+# Import PTS and CAMA from data library
 import pluto_pts &
 import pluto_input_cama_dof &
 import pluto_input_numbldgs &
 import pluto_input_geocodes &
+
+# Import spatial bounaries from data library
+import dcp_cdboundaries &
+import dcp_censustracts &
+import dcp_censusblocks &
+import dcp_schooldistricts &
+import dcp_firecompanies &
+import dcp_policeprecincts &
+import dcp_healthareas &
+import dcp_healthcenters &
+import dof_shoreline &
+
+# Import zoning files from data library
+import dcp_commercialoverlay &
+import dcp_limitedheight &
+import dcp_zoningdistricts &
+import dcp_specialpurpose &
+import dcp_specialpurposesubdistricts &
+import dcp_zoningmapamendments &
+import dcp_zoningmapindex &
+
+# Import other
+import dcp_colp &
+import dof_dtm &
+import dof_condo
+
 wait
 
 ## load data into the pluto db
