c75d8888ca38b1980be10ea99ed92bc28dac8fed
mgraber <mgraber@planning.nyc.gov>
2021-03-30 12:30:56 +0000

    Switch dataloading from fastloading.py to import from data library

--- pluto_build/python/fastloading.py
+++ pluto_build/python/fastloading.py
@@ -14,41 +14,14 @@ if __name__ == "__main__":
     tables = [
         "pluto_corrections",
         "dcp_edesignation",
-        "dcp_colp",
         "lpc_historic_districts",
         "lpc_landmarks",
 
         # for spatial joins
-        "dcp_cdboundaries",
-        "dcp_censustracts",
-        "dcp_censusblocks",
-        "dcp_school_districts",
-        "dcp_councildistricts",
         "doitt_zipcodeboundaries",
-        "dcp_firecompanies",
-        "dcp_policeprecincts",
-        "dcp_healthareas",
-        "dcp_healthcenters",
         "dsny_frequencies",
         "dpr_greenthumb",
-        
-        # raw Digital Tax Map from DOF
-        "dof_dtm",
-        
-        # raw NYC shoreline file from DOF
-        "dof_shoreline",
-        
-        # raw DOF condo table
-        "dof_condo",
-        
-        # DCP zoning datasets~
-        "dcp_commercialoverlay",
-        "dcp_limitedheight",
-        "dcp_zoningdistricts",
-        "dcp_specialpurpose",
-        "dcp_specialpurposesubdistricts",
-        "dcp_zoningmapamendments",
-        "dcp_zoningmapindex",
+        "dcp_councildistricts",
         
         # FEMA 2007 and preliminary 2015 100 year flood zones
         "fema_firms2007_100yr",
