644cde3b18c89d0817f192f0a7d013918b91b508
sptkl <caobaiyue@gmail.com>
2021-09-14 15:16:03 -0400

    update geocoding process to include 2020 census tract

--- pluto_build/python/geocode.py
+++ pluto_build/python/geocode.py
@@ -7,6 +7,7 @@ import os
 
 g = Geosupport()
 
+
 def get_address(bbl):
     try:
         geo = g["BL"](bbl=bbl)
@@ -21,7 +22,8 @@ def get_address(bbl):
             "5-Digit Street Code", "00000"
         )
         sname = get_sname(b5sc)
-        numberOfExistingStructures = geo.get("Number of Existing Structures on Lot", "")
+        numberOfExistingStructures = geo.get(
+            "Number of Existing Structures on Lot", "")
         hnum = address.get("Low House Number", "")
         return dict(
             sname=sname,
@@ -53,25 +55,25 @@ def geocode(inputs):
     hnum = address.get("hnum", "")
     numberOfExistingStructures = address.get("numberOfExistingStructures", "")
     borough = boro
-
+    extra = dict(
+        borough=borough,
+        block=block,
+        lot=lot,
+        easement=ease,
+        input_hnum=hnum,
+        input_sname=sname,
+        numberOfExistingStructures=numberOfExistingStructures,
+    )
     try:
         geo1 = g["1A"](
             street_name=sname, house_number=hnum, borough=borough, mode="regular"
         )
         geo2 = g["1E"](
-            street_name=sname, house_number=hnum, borough=borough, mode="regular"
+            street_name=sname, house_number=hnum, borough=borough, mode="extended"
         )
         geo = {**geo1, **geo2}
         geo = parse_output(geo)
-        geo.update(
-            borough=borough,
-            block=block,
-            lot=lot,
-            easement=ease,
-            input_hnum=hnum,
-            input_sname=sname,
-            numberOfExistingStructures=numberOfExistingStructures,
-        )
+        geo.update(extra)
         return geo
     except GeosupportError:
         try:
@@ -79,53 +81,32 @@ def geocode(inputs):
                 street_name=sname, house_number=hnum, borough=borough, mode="regular"
             )
             geo = parse_output(geo)
-            geo.update(
-                borough=borough,
-                block=block,
-                lot=lot,
-                easement=ease,
-                input_hnum=hnum,
-                input_sname=sname,
-                numberOfExistingStructures=numberOfExistingStructures,
-            )
+            geo.update(extra)
             return geo
         except GeosupportError as e1:
             try:
                 geo = g["BL"](bbl=bbl)
                 geo = parse_output(geo)
-                geo.update(
-                    borough=borough,
-                    block=block,
-                    lot=lot,
-                    easement=ease,
-                    input_hnum=hnum,
-                    input_sname=sname,
-                    numberOfExistingStructures=numberOfExistingStructures,
-                )
+                geo.update(extra)
                 return geo
             except GeosupportError as e2:
                 geo = parse_output(e1.result)
-                geo.update(
-                    borough=borough,
-                    block=block,
-                    lot=lot,
-                    easement=ease,
-                    input_hnum=hnum,
-                    input_sname=sname,
-                    numberOfExistingStructures=numberOfExistingStructures,
-                )
+                geo.update(extra)
                 return geo
 
 
 def parse_output(geo):
     return dict(
         billingbbl=geo.get("Condominium Billing BBL", ""),
-        bbl=geo.get("BOROUGH BLOCK LOT (BBL)", "").get("BOROUGH BLOCK LOT (BBL)", ""),
+        bbl=geo.get("BOROUGH BLOCK LOT (BBL)", "").get(
+            "BOROUGH BLOCK LOT (BBL)", ""),
         communityDistrict=geo.get("COMMUNITY DISTRICT", {}).get(
             "COMMUNITY DISTRICT", ""
         ),
         censusTract2010=geo.get("2010 Census Tract", ""),
         censusBlock2010=geo.get("2010 Census Block", ""),
+        censusTract2020=geo.get("2020 Census Tract", ""),
+        censusBlock2020=geo.get("2020 Census Block", ""),
         communitySchoolDistrict=geo.get("Community School District", ""),
         cityCouncilDistrict=geo.get("City Council District", ""),
         zipCode=geo.get("ZIP Code", ""),
@@ -139,8 +120,8 @@ def parse_output(geo):
             "Sanitation Collection Scheduling Section and Subsection", ""
         ),
         boePreferredStreetName=geo.get("BOE Preferred Street Name", ""),
-        # numberOfExistingStructures = geo.get('Number of Existing Structures on Lot', ''),
-        taxMapNumberSectionAndVolume=geo.get("Tax Map Number Section & Volume", ""),
+        taxMapNumberSectionAndVolume=geo.get(
+            "Tax Map Number Section & Volume", ""),
         sanbornMapIdentifier=geo.get("SBVP (SANBORN MAP IDENTIFIER)", {}).get(
             "SBVP (SANBORN MAP IDENTIFIER)", ""
         ),
