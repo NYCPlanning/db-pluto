a0659b4d235acb1ea9645f11aeea14b12298ecaf
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2021-12-02 09:42:42 -0500

    adding corr_units.sql to shell script

--- pluto_build/03_corrections.sh
+++ pluto_build/03_corrections.sh
@@ -9,6 +9,7 @@ psql $BUILD_ENGINE -f sql/corr_yearbuilt_lpc.sql
 psql $BUILD_ENGINE -f sql/corr_ownername_city.sql
 psql $BUILD_ENGINE -f sql/corr_communitydistrict.sql
 psql $BUILD_ENGINE -f sql/corr_numfloors.sql
+psql $BUILD_ENGINE -f sql/corr_units.sql
 psql $BUILD_ENGINE -f sql/corr_inwoodrezoning.sql
 psql $BUILD_ENGINE -f sql/corr_dropoldrecords.sql
 psql $BUILD_ENGINE -f sql/remove_unitlots.sql
