8d40ebf458a2e2438b4e33a6696e5b782cdadd90
Sasha Weinstein <70385853+SashaWeinstein@users.noreply.github.com>
2022-05-05 15:10:23 -0400

    all field comparisons switched to count NULL and non-NULL as different. bbl comparison taken away

--- pluto_build/sql/qaqc_mismatch.sql
+++ pluto_build/sql/qaqc_mismatch.sql
@@ -51,63 +51,97 @@ SELECT
     count(*) FILTER (WHERE a.easements::numeric IS DISTINCT FROM b.easements::numeric) as easements,
     count(*) FILTER (WHERE a.ownertype IS DISTINCT FROM b.ownertype) as ownertype,
     count(*) FILTER (WHERE a.ownername IS DISTINCT FROM b.ownername) as ownername,
-    count(*) FILTER (WHERE abs(a.lotarea::int - b.lotarea::int)>=5 OR 
+    count(*) FILTER (WHERE abs(a.lotarea::numeric - b.lotarea::numeric)>=5 OR 
         ((a.lotarea IS NULL)::int + (b.lotarea IS NULL)::int) = 1) as lotarea,
-    count(nullif(abs(a.bldgarea::numeric-b.bldgarea::numeric) <5, true)) as bldgarea,
-    count(nullif(abs(a.comarea::numeric-b.comarea::numeric) <5, true)) as comarea,
-    count(nullif(abs(a.resarea::numeric-b.resarea::numeric) <5, true)) as resarea,
-    count(nullif(abs(a.officearea::numeric-b.officearea::numeric) <5, true)) as officearea,
-    count(nullif(abs(a.retailarea::numeric-b.retailarea::numeric) <5, true)) as retailarea,
-    count(nullif(abs(a.garagearea::numeric-b.garagearea::numeric) <5, true)) as garagearea,
-    count(nullif(abs(a.strgearea::numeric-b.strgearea::numeric) <5, true)) as strgearea,
-    count(nullif(abs(a.factryarea::numeric-b.factryarea::numeric) <5, true)) as factryarea,
-    count(nullif(abs(a.otherarea::numeric-b.otherarea::numeric) <5, true)) as otherarea,
+    count(*) FILTER (WHERE abs(a.bldgarea::numeric - b.bldgarea::numeric)>=5 OR 
+        ((a.bldgarea IS NULL)::int + (b.bldgarea IS NULL)::int) = 1) as bldgarea,
+    count(*) FILTER (WHERE abs(a.comarea::numeric - b.comarea::numeric)>=5 OR 
+        ((a.comarea IS NULL)::int + (b.comarea IS NULL)::int) = 1) as comarea,
+    count(*) FILTER (WHERE abs(a.resarea::numeric - b.resarea::numeric)>=5 OR 
+        ((a.resarea IS NULL)::int + (b.resarea IS NULL)::int) = 1) as resarea,
+    count(*) FILTER (WHERE abs(a.officearea::numeric - b.officearea::numeric)>=5 OR 
+        ((a.officearea IS NULL)::int + (b.officearea IS NULL)::int) = 1) as officearea,
+    count(*) FILTER (WHERE abs(a.retailarea::numeric - b.retailarea::numeric)>=5 OR 
+        ((a.retailarea IS NULL)::int + (b.retailarea IS NULL)::int) = 1) as retailarea,
+    count(*) FILTER (WHERE abs(a.garagearea::numeric - b.garagearea::numeric)>=5 OR 
+        ((a.garagearea IS NULL)::int + (b.garagearea IS NULL)::int) = 1) as garagearea,
+    count(*) FILTER (WHERE abs(a.strgearea::numeric - b.strgearea::numeric)>=5 OR 
+        ((a.strgearea IS NULL)::int + (b.strgearea IS NULL)::int) = 1) as strgearea,
+    count(*) FILTER (WHERE abs(a.factryarea::numeric - b.factryarea::numeric)>=5 OR 
+        ((a.factryarea IS NULL)::int + (b.factryarea IS NULL)::int) = 1) as factryarea,
+    count(*) FILTER (WHERE abs(a.otherarea::numeric - b.otherarea::numeric)>=5 OR 
+        ((a.otherarea IS NULL)::int + (b.otherarea IS NULL)::int) = 1) as otherarea,
     count(*) FILTER (WHERE a.areasource IS DISTINCT FROM b.areasource) as areasource,
-    count(nullif(a.numbldgs::numeric = b.numbldgs::numeric, true)) as numbldgs,
-    count(nullif(a.numfloors::numeric = b.numfloors::numeric, true)) as numfloors,
-    count(nullif(a.unitsres::numeric = b.unitsres::numeric, true)) as unitsres,
-    count(nullif(a.unitstotal::numeric = b.unitstotal::numeric, true)) as unitstotal,
-    count(nullif(abs(a.lotfront::numeric-b.lotfront::numeric) <5, true)) as lotfront,
-    count(nullif(abs(a.lotdepth::numeric-b.lotdepth::numeric) <5, true)) as lotdepth,
-    count(nullif(abs(a.bldgfront::numeric-b.bldgfront::numeric) <5, true)) as bldgfront,
-    count(nullif(abs(a.bldgdepth::numeric-b.bldgdepth::numeric) <5, true)) as bldgdepth,
-    count(nullif(a.ext = b.ext, true)) as ext,
-    count(nullif(a.proxcode = b.proxcode, true)) as proxcode,
-    count(nullif(a.irrlotcode = b.irrlotcode, true)) as irrlotcode,
-    count(nullif(a.lottype = b.lottype, true)) as lottype,
-    count(nullif(a.bsmtcode = b.bsmtcode, true)) as bsmtcode,
-    count(nullif(abs(a.assessland::numeric-b.assessland::numeric) <10, true)) as assessland,
-    count(nullif(abs(a.assesstot::numeric-b.assesstot::numeric) <10, true)) as assesstot,
-    count(nullif(abs(a.exempttot::numeric-b.exempttot::numeric) <10, true)) as exempttot,
-    count(nullif(a.yearbuilt::numeric = b.yearbuilt::numeric, true)) as yearbuilt,
-    count(nullif(a.yearalter1::numeric = b.yearalter1::numeric, true)) as yearalter1,
-    count(nullif(a.yearalter2::numeric = b.yearalter2::numeric, true)) as yearalter2,
-    count(nullif(a.histdist = b.histdist, true)) as histdist,
-    count(nullif(a.landmark = b.landmark, true)) as landmark,
-    count(nullif(a.builtfar::double precision = b.builtfar::double precision, true)) as builtfar,
-    count(nullif(a.residfar::double precision = b.residfar::double precision, true)) as residfar,
-    count(nullif(a.commfar::double precision = b.commfar::double precision, true)) as commfar,
-    count(nullif(a.facilfar::double precision = b.facilfar::double precision, true)) as facilfar,
-    count(nullif(a.borocode::numeric = b.borocode::numeric, true)) as borocode,
+    count(*) FILTER (WHERE a.numbldgs::numeric IS DISTINCT FROM b.numbldgs::numeric) 
+    as numbldgs,
+    count(*) FILTER (WHERE a.numfloors::numeric IS DISTINCT FROM b.numfloors::numeric)
+    as numfloors,
+    count(*) FILTER (WHERE a.unitsres::numeric IS DISTINCT FROM b.unitsres::numeric) 
+    as unitsres,
+    count(*) FILTER (WHERE a.unitstotal::numeric IS DISTINCT FROM b.unitstotal::numeric) 
+    as unitstotal,
+    count(*) FILTER (WHERE abs(a.lotfront::numeric - b.lotfront::numeric)>=5 OR 
+        ((a.lotfront IS NULL)::int + (b.lotfront IS NULL)::int) = 1) as lotfront,
+    count(*) FILTER (WHERE abs(a.lotdepth::numeric - b.lotdepth::numeric)>=5 OR 
+        ((a.lotdepth IS NULL)::int + (b.lotdepth IS NULL)::int) = 1) as lotdepth,
+    count(*) FILTER (WHERE abs(a.bldgfront::numeric - b.bldgfront::numeric)>=5 OR 
+        ((a.bldgfront IS NULL)::int + (b.bldgfront IS NULL)::int) = 1) as bldgfront,
+    count(*) FILTER (WHERE abs(a.bldgdepth::numeric - b.bldgdepth::numeric)>=5 OR 
+        ((a.bldgdepth IS NULL)::int + (b.bldgdepth IS NULL)::int) = 1) as bldgdepth,
+    count(*) FILTER (WHERE a.ext IS DISTINCT FROM b.ext) as ext,
+    count(*) FILTER (WHERE a.proxcode IS DISTINCT FROM b.proxcode) as proxcode,
+    count(*) FILTER (WHERE a.irrlotcode IS DISTINCT FROM b.irrlotcode)  as irrlotcode,
+    count(*) FILTER (WHERE a.lottype IS DISTINCT FROM b.lottype) as lottype,
+    count(*) FILTER (WHERE a.bsmtcode IS DISTINCT FROM b.bsmtcode)  as bsmtcode,
+    count(*) FILTER (WHERE abs(a.assessland::numeric - b.assessland::numeric)>=10 OR 
+        ((a.assessland IS NULL)::int + (b.assessland IS NULL)::int) = 1) as assessland,
+    count(*) FILTER (WHERE abs(a.assesstot::numeric - b.assesstot::numeric)>=10 OR 
+        ((a.assesstot IS NULL)::int + (b.assesstot IS NULL)::int) = 1) as assesstot,
+    count(*) FILTER (WHERE abs(a.exempttot::numeric - b.exempttot::numeric)>=10 OR 
+        ((a.exempttot IS NULL)::int + (b.exempttot IS NULL)::int) = 1) as exempttot,
+    count(*) FILTER (WHERE a.yearbuilt::numeric IS DISTINCT FROM b.yearbuilt::numeric)  
+    as yearbuilt,
+    count(*) FILTER (WHERE a.yearalter1::numeric IS DISTINCT FROM b.yearalter1::numeric) 
+    as yearalter1,
+    count(*) FILTER (WHERE a.yearalter2::numeric IS DISTINCT FROM b.yearalter2::numeric) 
+    as yearalter2,
+    count(*) FILTER (WHERE a.histdist IS DISTINCT FROM b.histdist) as histdist,
+    count(*) FILTER (WHERE a.landmark IS DISTINCT FROM b.landmark) as landmark,
+    count(*) FILTER (WHERE a.builtfar::double precision IS DISTINCT FROM 
+    b.builtfar::double precision) as builtfar,
+    count(*) FILTER (WHERE a.residfar::double precision IS DISTINCT FROM 
+    b.residfar::double precision) as residfar,
+    count(*) FILTER (WHERE a.commfar::double precision IS DISTINCT FROM 
+    b.commfar::double precision) as commfar,
+    count(*) FILTER (WHERE a.facilfar::double precision IS DISTINCT FROM 
+    b.facilfar::double precision) as facilfar,
+    count(*) FILTER (WHERE a.borocode::numeric IS DISTINCT FROM  b.borocode::numeric) 
+    as borocode,
     count(nullif(a.bbl::double precision = b.bbl::double precision, true)) as bbl,
-    count(nullif(a.condono::numeric = b.condono::numeric, true)) as condono,
-    count(nullif(a.tract2010 = b.tract2010, true)) as tract2010,
-    count(nullif(abs(a.xcoord::numeric-b.xcoord::numeric) <1, true)) as xcoord,
-    count(nullif(abs(a.ycoord::numeric-b.ycoord::numeric) <1, true)) as ycoord,
-    count(nullif(abs(a.latitude::numeric-b.latitude::numeric) <0.0001, true)) as latitude,
-    count(nullif(abs(a.longitude::numeric-b.longitude::numeric) <0.0001, true)) as longitude,
-    count(nullif(a.zonemap = b.zonemap, true)) as zonemap,
-    count(nullif(a.zmcode = b.zmcode, true)) as zmcode,
-    count(nullif(a.sanborn = b.sanborn, true)) as sanborn,
-    count(nullif(a.taxmap = b.taxmap, true)) as taxmap,
-    count(nullif(a.edesignum = b.edesignum, true)) as edesignum,
-    count(nullif(a.appbbl::double precision = b.appbbl::double precision, true)) as appbbl,
-    count(nullif(a.appdate = b.appdate, true)) as appdate,
-    count(nullif(a.plutomapid = b.plutomapid, true)) as plutomapid,
-    count(nullif(a.sanitdistrict = b.sanitdistrict, true)) as sanitdistrict,
-    count(nullif(a.healthcenterdistrict::numeric = b.healthcenterdistrict::numeric, true)) as healthcenterdistrict,
-    count(nullif(a.firm07_flag = b.firm07_flag, true)) as firm07_flag,
-    count(nullif(a.pfirm15_flag = b.pfirm15_flag, true)) as pfirm15_flag
+    count(*) FILTER (WHERE a.condono::numeric IS DISTINCT FROM b.condono::numeric) as condono,
+    count(*) FILTER (WHERE a.tract2010 IS DISTINCT FROM b.tract2010) as tract2010,
+    count(*) FILTER (WHERE abs(a.xcoord::numeric - b.xcoord::numeric)>=1 OR 
+        ((a.xcoord IS NULL)::int + (b.xcoord IS NULL)::int) = 1) as xcoord,
+    count(*) FILTER (WHERE abs(a.ycoord::numeric - b.ycoord::numeric)>=1 OR 
+        ((a.ycoord IS NULL)::int + (b.ycoord IS NULL)::int) = 1) as ycoord,
+    count(*) FILTER (WHERE abs(a.latitude::numeric - b.latitude::numeric)>=.0001 OR 
+        ((a.latitude IS NULL)::int + (b.latitude IS NULL)::int) = 1) as latitude,
+    count(*) FILTER (WHERE abs(a.longitude::numeric - b.longitude::numeric)>=.0001 OR 
+        ((a.longitude IS NULL)::int + (b.longitude IS NULL)::int) = 1) as longitude,
+    count(*) FILTER (WHERE a.zonemap IS DISTINCT FROM b.zonemap) as zonemap,
+    count(*) FILTER (WHERE a.zmcode IS DISTINCT FROM b.zmcode) as zmcode,
+    count(*) FILTER (WHERE a.sanborn IS DISTINCT FROM b.sanborn) as sanborn,
+    count(*) FILTER (WHERE a.taxmap IS DISTINCT FROM b.taxmap) as taxmap,
+    count(*) FILTER (WHERE a.edesignum IS DISTINCT FROM b.edesignum) as edesignum,
+    count(*) FILTER (WHERE a.appbbl::double precision 
+        IS DISTINCT FROM b.appbbl::double precision) as appbbl,
+    count(*) FILTER (WHERE a.appdate IS DISTINCT FROM b.appdate) as appdate,
+    count(*) FILTER (WHERE a.plutomapid IS DISTINCT FROM b.plutomapid)  as plutomapid,
+    count(*) FILTER (WHERE a.sanitdistrict IS DISTINCT FROM b.sanitdistrict) as sanitdistrict,
+    count(*) FILTER (WHERE a.healthcenterdistrict::numeric 
+        IS DISTINCT FROM b.healthcenterdistrict::numeric) as healthcenterdistrict,
+    count(*) FILTER (WHERE a.firm07_flag IS DISTINCT FROM b.firm07_flag) as firm07_flag,
+    count(*) FILTER (WHERE a.pfirm15_flag IS DISTINCT FROM b.pfirm15_flag)  as pfirm15_flag
     INTO qaqc_mismatch
     FROM export_pluto a
 INNER JOIN dcp_pluto b
