60fbd9ab9b91aadc6b582153d5bcff3d61197427
Te Du <tedudevelop@gmail.com>
2022-05-10 10:21:40 -0400

    delete postgres installation

--- pluto_build/bin/setup.sh
+++ pluto_build/bin/setup.sh
@@ -4,17 +4,8 @@ function setup {
 sudo apt update
 sudo apt install -y curl zip
 
-sudo tee /etc/apt/sources.list.d/pgdg.list << END
-deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main
-END
-# get the signing key and import it
-curl -O https://www.postgresql.org/media/keys/ACCC4CF8.asc
-sudo apt-key add ACCC4CF8.asc
-
-sudo apt update
-sudo apt install -y postgresql-client-11 gdal-bin
-sudo apt autoremove -y
-rm ACCC4CF8.asc
+# install pip for the workflow to run correctly
+sudo apt install python3-pip
 
 curl -O https://dl.min.io/client/mc/release/linux-amd64/mc
 chmod +x mc
