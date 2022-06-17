57244f672b4f6106a8603b9a4d4c4e4c49fc5f8c
Andrew Brieff <abreff@gmail.com>
2022-06-17 14:57:29 -0400

    add descriptive comment

--- pluto_build/sql/corr_ownername_punctuation.sql
+++ pluto_build/sql/corr_ownername_punctuation.sql
@@ -1,3 +1,5 @@
+-- Where the ownername has invalid leading punctuation, remove it and set dcpedited flag to true
+
 UPDATE pluto a
 SET ownername = trim(regexp_replace(a.ownername, '^([.,;><\-_!?`%]+)(.*)', '\2')),
     dcpedited= 't'
