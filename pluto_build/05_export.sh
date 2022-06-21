1c1027fbba30dbfcac5e267055f33493ebf2aa49
Andrew Brieff <abreff@gmail.com>
2022-06-21 14:53:04 -0400

    rebased

--- pluto_build/05_export.sh
+++ pluto_build/05_export.sh
@@ -20,39 +20,39 @@ mkdir -p output &&
 # FGDB_export mappluto_gdb &
 
 # mappluto_unclipped.gdb
-FGDB_export mappluto_unclipped_gdb_test &
+FGDB_export mappluto_unclipped_gdb &
 
 # # mappluto
-# SHP_export mappluto &
+SHP_export mappluto &
 
 # # mappluto_unclipped
 # SHP_export mappluto_unclipped &
 
 # Pluto
-mkdir -p output/pluto &&
-  (cd output/pluto
-    rm -f pluto.zip
-    psql $BUILD_ENGINE -c "\COPY ( 
-          SELECT * FROM export_pluto
-        ) TO STDOUT DELIMITER ',' CSV HEADER;" > pluto.csv
-    echo "$VERSION" > version.txt
-    echo "$(wc -l pluto.csv)" >> version.txt
-    zip pluto.zip *
-    ls | grep -v pluto.zip | xargs rm
-  )
+# mkdir -p output/pluto &&
+#   (cd output/pluto
+#     rm -f pluto.zip
+#     psql $BUILD_ENGINE -c "\COPY ( 
+#           SELECT * FROM export_pluto
+#         ) TO STDOUT DELIMITER ',' CSV HEADER;" > pluto.csv
+#     echo "$VERSION" > version.txt
+#     echo "$(wc -l pluto.csv)" >> version.txt
+#     zip pluto.zip *
+#     ls | grep -v pluto.zip | xargs rm
+#   )
 
-# BBL and Council info for DOF
-mkdir -p output/dof && 
-  (cd output/dof
-    rm -f bbl_council.zip
-    psql $BUILD_ENGINE -c "\COPY ( 
-          SELECT bbl, council FROM export_pluto
-          WHERE bbl is not null
-        ) TO STDOUT DELIMITER ',' CSV HEADER;" > bbl_council.csv
-    echo "$VERSION" > version.txt
-    zip bbl_council.zip *
-    ls | grep -v bbl_council.zip | xargs rm
-  )
+# # BBL and Council info for DOF
+# mkdir -p output/dof && 
+#   (cd output/dof
+#     rm -f bbl_council.zip
+#     psql $BUILD_ENGINE -c "\COPY ( 
+#           SELECT bbl, council FROM export_pluto
+#           WHERE bbl is not null
+#         ) TO STDOUT DELIMITER ',' CSV HEADER;" > bbl_council.csv
+#     echo "$VERSION" > version.txt
+#     zip bbl_council.zip *
+#     ls | grep -v bbl_council.zip | xargs rm
+#   )
 
 wait
 
