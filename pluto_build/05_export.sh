900301c2f9c76606d9424b9fedf308f7d222b284
Andrew Brieff <abreff@gmail.com>
2022-06-21 13:56:35 -0400

    remove extra comments'

--- pluto_build/05_export.sh
+++ pluto_build/05_export.sh
@@ -28,31 +28,31 @@ SHP_export mappluto &
 # # mappluto_unclipped
 # SHP_export mappluto_unclipped &
 
-# Pluto
-# mkdir -p output/pluto &&
-#   (cd output/pluto
-#     rm -f pluto.zip
-#     psql $BUILD_ENGINE -c "\COPY ( 
-#           SELECT * FROM export_pluto
-#         ) TO STDOUT DELIMITER ',' CSV HEADER;" > pluto.csv
-#     echo "$VERSION" > version.txt
-#     echo "$(wc -l pluto.csv)" >> version.txt
-#     zip pluto.zip *
-#     ls | grep -v pluto.zip | xargs rm
-#   )
+Pluto
+mkdir -p output/pluto &&
+  (cd output/pluto
+    rm -f pluto.zip
+    psql $BUILD_ENGINE -c "\COPY ( 
+          SELECT * FROM export_pluto
+        ) TO STDOUT DELIMITER ',' CSV HEADER;" > pluto.csv
+    echo "$VERSION" > version.txt
+    echo "$(wc -l pluto.csv)" >> version.txt
+    zip pluto.zip *
+    ls | grep -v pluto.zip | xargs rm
+  )
 
-# # BBL and Council info for DOF
-# mkdir -p output/dof && 
-#   (cd output/dof
-#     rm -f bbl_council.zip
-#     psql $BUILD_ENGINE -c "\COPY ( 
-#           SELECT bbl, council FROM export_pluto
-#           WHERE bbl is not null
-#         ) TO STDOUT DELIMITER ',' CSV HEADER;" > bbl_council.csv
-#     echo "$VERSION" > version.txt
-#     zip bbl_council.zip *
-#     ls | grep -v bbl_council.zip | xargs rm
-#   )
+# BBL and Council info for DOF
+mkdir -p output/dof && 
+  (cd output/dof
+    rm -f bbl_council.zip
+    psql $BUILD_ENGINE -c "\COPY ( 
+          SELECT bbl, council FROM export_pluto
+          WHERE bbl is not null
+        ) TO STDOUT DELIMITER ',' CSV HEADER;" > bbl_council.csv
+    echo "$VERSION" > version.txt
+    zip bbl_council.zip *
+    ls | grep -v bbl_council.zip | xargs rm
+  )
 
 wait
 
