bad26540c83413d1931ffbbfb3d7e3b5b9cfb73e
Te Du <tedudevelop@gmail.com>
2022-06-21 17:15:32 -0400

    add docker compose command to set up

--- pluto_build/bin/setup.sh
+++ pluto_build/bin/setup.sh
@@ -8,5 +8,7 @@ curl -O https://dl.min.io/client/mc/release/linux-amd64/mc
 chmod +x mc
 sudo mv ./mc /usr/bin
 mc config host add spaces $AWS_S3_ENDPOINT $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY --api S3v4
+
+docker-compose up -d
 }
 register 'setup' 'init' 'install all dependencies' setup
