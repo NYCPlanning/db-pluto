c3d69123c422809c2a182c1be77fcee0928cd7cc
Te Du <tedudevelop@gmail.com>
2022-06-23 18:13:39 -0400

    move location for pluto.sh to the bin following other github action template

--- pluto_build/bin/pluto.sh
+++ pluto_build/bin/pluto.sh
@@ -6,3 +6,7 @@ function dataloading {
     docker compose exec -T ./01_dataloading.sh
 }
 register 'build ''dataloading' 'build dataloading' dataloading
+
+case $1 in 
+    dataloading) dataloading
+esac
