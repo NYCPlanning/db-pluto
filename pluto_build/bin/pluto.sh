0623baffb8d1e6bb3422b9a7786cce6008af6345
Te Du <tedudevelop@gmail.com>
2022-06-23 18:26:49 -0400

    add more argument and call with container

--- pluto_build/bin/pluto.sh
+++ pluto_build/bin/pluto.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 function dataloading {
-    docker compose exec -T ./01_dataloading.sh
+    docker compose exec -T pluto ./01_dataloading.sh
 }
 register 'build ''dataloading' 'build dataloading' dataloading
 
 case $1 in 
-    dataloading) dataloading
+    dataloading) dataloading ;;
 esac
