b58fec7558212047f8571c8671242d2c86d37a0b
Te Du <tedudevelop@gmail.com>
2022-06-23 17:29:20 -0400

    try run bash script

--- pluto_build/bin/setup.sh
+++ pluto_build/bin/setup.sh
@@ -11,7 +11,5 @@ function setup {
     chmod +x mc
     sudo mv ./mc /usr/bin
     mc config host add spaces $AWS_S3_ENDPOINT $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY --api S3v4
-
-    docker-compose up -d
 }
 register 'setup' 'init' 'install all dependencies' setup
