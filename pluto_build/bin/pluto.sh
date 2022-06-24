24b8d9f414278fc657dd8be8eb47c34a0f24e923
Te Du <tedudevelop@gmail.com>
2022-06-24 11:45:04 -0400

    edit pluto.sh commands

--- pluto_build/bin/pluto.sh
+++ pluto_build/bin/pluto.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
 function dataloading {
-    docker compose exec -T pluto pwd
-    docker compose exec -T pluto ./work/pluto_build/01_dataloading.sh
+    docker compose exec -T pluto /src/pluto_build/01_dataloading.sh
 }
 register 'build' 'dataloading' 'build dataloading' dataloading
 
+function build {
+    docker compose exec -T pluto /src/pluto_build/02_build.sh
+}
+register 'build' 'build' 'build build' build
+
+function corrections {
+        docker compose exec -T pluto /src/pluto_build/03_corrections.sh
+}
+register 'build' 'corrections' 'build corrections' build
+
+function archive {
+        docker compose exec -T pluto /src/pluto_build/04_archive.sh
+}
+register 'build' 'archive' 'build archive' build
+
+function corrections {
+        docker compose exec -T pluto /src/pluto_build/03_corrections.sh
+}
+register 'build' 'corrections' 'build corrections' build
+
+
 case $1 in 
     dataloading) dataloading ;;
+    build) build ;;
+    corrections) corrections;;
+    archive) archive;;
+    export) export;;
+    upload) upload;;
 esac
