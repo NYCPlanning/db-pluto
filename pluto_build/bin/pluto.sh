62c3c3c646b09afd3e9f3877ce8fd10b31645e66
Te Du <tedudevelop@gmail.com>
2022-06-24 09:43:33 -0400

    try running pwd

--- pluto_build/bin/pluto.sh
+++ pluto_build/bin/pluto.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 function dataloading {
+    docker compose exec -T pluto pwd
     docker compose exec -T pluto ./pluto_build/01_dataloading.sh
 }
 register 'build' 'dataloading' 'build dataloading' dataloading
