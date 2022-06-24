6b55e659344379f8f53fb0fe95ace5ccd0fc8a7d
Te Du <tedudevelop@gmail.com>
2022-06-24 09:23:29 -0400

    spacing off for bash script

--- pluto_build/bin/pluto.sh
+++ pluto_build/bin/pluto.sh
@@ -2,7 +2,7 @@
 function dataloading {
     docker compose exec -T pluto ./01_dataloading.sh
 }
-register 'build ''dataloading' 'build dataloading' dataloading
+register 'build' 'dataloading' 'build dataloading' dataloading
 
 case $1 in 
     dataloading) dataloading ;;
