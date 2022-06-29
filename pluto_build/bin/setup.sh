d668e4826599e28dc704e6ae2c842d550840d1ea
Andrew Brieff <abreff@gmail.com>
2022-06-29 15:20:10 -0400

    Try moving mc code to dockerfile

--- pluto_build/bin/setup.sh
+++ pluto_build/bin/setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 function setup {
-    /usr/bin/mc config host add spaces $AWS_S3_ENDPOINT $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY --api S3v4
+    curl -O https://dl.min.io/client/mc/release/linux-amd64/mc
+    chmod +x mc
+    mv ./mc /usr/bin
+    mc config host add spaces $AWS_S3_ENDPOINT $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY --api S3v4
 }
 register 'setup' 'init' 'install all dependencies' setup
