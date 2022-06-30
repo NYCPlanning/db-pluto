10cc714e5508a339071558e729a4819cad32f7fd
Andrew Brieff <abreff@gmail.com>
2022-06-30 10:18:53 -0400

    fix

--- pluto_build/bin/setup.sh
+++ pluto_build/bin/setup.sh
@@ -8,4 +8,3 @@ function setup {
     whoami
     mc config host add spaces $AWS_S3_ENDPOINT $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY --api S3v4
 }
-register 'setup' 'init' 'install all dependencies' setup
