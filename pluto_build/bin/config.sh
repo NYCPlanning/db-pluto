f669baffb6381e906d2adb424fa862d276c7d364
Andrew Brieff <abreff@gmail.com>
2022-06-30 10:06:19 -0400

    fix

--- pluto_build/bin/config.sh
+++ pluto_build/bin/config.sh
@@ -83,7 +83,7 @@ function imports_csv {
 function Upload {
   echo "running upload"
   which mc
-  ls /usr/bin
+  whoami
   # mc cp pluto_pts.csv spaces/edm-recipes/tmp/pluto_pts.csv
   # mc rm -r --force spaces/edm-publishing/db-pluto/$@
   # mc cp -r output spaces/edm-publishing/db-pluto/$@
