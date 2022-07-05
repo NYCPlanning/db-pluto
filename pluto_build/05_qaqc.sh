7354f6d895b0e4e9bf1e40161faf300b4a2834e2
Sasha Weinstein <70385853+SashaWeinstein@users.noreply.github.com>
2022-07-05 16:29:05 -0400

    refactor QAQC bash code to be more DRY

--- pluto_build/05_qaqc.sh
+++ pluto_build/05_qaqc.sh
@@ -1,111 +1,49 @@
 #!/bin/bash
 source bin/config.sh
 
-# # QAQC EXPECTED VALUE ANALYSIS
-# psql $BUILD_ENGINE \
-#   -v VERSION=$VERSION \
-#   -f sql/qaqc_expected.sql &
+# QAQC EXPECTED VALUE ANALYSIS
+psql $BUILD_ENGINE \
+  -v VERSION=$VERSION \
+  -f sql/qaqc_expected.sql &
+
+function set_condition {
+  mapped=$1 
+  condo=$2
+  if [ "${mapped}" = true ] && [ "${condo}" = true ] ; then
+    export condition="WHERE right(a.bbl::bigint::text, 4) LIKE '75%%' AND a.geom IS NOT NULL"
+  elif [ "${mapped}" = true ] && [ "${condo}" = false ] ; then
+    export condition="WHERE a.geom IS NOT NULL"
+  elif [ "${mapped}" = false ] && [ "${condo}" = true ] ; then
+    export condition="WHERE right(a.bbl::bigint::text, 4) LIKE '75%%'"
+  elif [ "${mapped}" = false ] && [ "${condo}" = false ] ; then
+    export condition=""
+  fi
+}
+
+function QAQC {
+  echo "Running QAQC Mismatch"
+  echo "file = $1, mapped = $2, condo = $3"
+  file=$1
+  mapped=$2
+  condo=$3
+  set_condition $mapped $condo
+  echo "condition is $condition"
+  args="-v VERSION=$VERSION -v VERSION_PREV=$VERSION_PREV -v CONDO=$condo \
+  -v MAPPED=$mapped "
+  psql $BUILD_ENGINE -v VERSION=$VERSION -v VERSION_PREV=$VERSION_PREV -v CONDO=$condo \
+  -v MAPPED=$mapped  -v CONDITION="$condition" -f $file
+}
+
+
 
 # QAQC MISMATCH ANALYSIS
-echo "Running QAQC Mismatch"
-echo "Condo=true, mapped=false, condition is bbl ends with 75%%"
-psql $BUILD_ENGINE \
-  -v VERSION=$VERSION \
-  -v VERSION_PREV=$VERSION_PREV \
-  -v CONDO='TRUE' \
-  -v MAPPED='FALSE'\
-  -v CONDITION="WHERE right(a.bbl::bigint::text, 4) LIKE '75%%'" \
-  -f sql/qaqc_mismatch.sql 
-
-echo "Condo=true, mapped=true, condition is bbl ends with 75%% and geom not null"
-psql $BUILD_ENGINE \
-  -v VERSION=$VERSION \
-  -v VERSION_PREV=$VERSION_PREV \
-  -v CONDO='TRUE' \
-  -v MAPPED='TRUE'\
-  -v CONDITION="WHERE right(a.bbl::bigint::text, 4) LIKE '75%%' AND a.geom IS NOT NULL" \
-  -f sql/qaqc_mismatch.sql 
-
-echo "Condo=false, mapped=false, no condition passed"
-psql $BUILD_ENGINE \
-  -v VERSION=$VERSION \
-  -v VERSION_PREV=$VERSION_PREV \
-  -v CONDO='FALSE' \
-  -v MAPPED='FALSE'\
-  -v CONDITION="" \
-  -f sql/qaqc_mismatch.sql 
-
-echo "Condo=false, mapped=true, condition is geom is not null"
-psql $BUILD_ENGINE \
-  -v VERSION=$VERSION \
-  -v VERSION_PREV=$VERSION_PREV \
-  -v CONDO='FALSE' \
-  -v MAPPED='TRUE'\
-  -v CONDITION="WHERE a.geom IS NOT NULL" \
-  -f sql/qaqc_mismatch.sql 
-
-# # QAQC NULL ANALYSIS
-# psql $BUILD_ENGINE \
-#   -v VERSION=$VERSION \
-#   -v VERSION_PREV=$VERSION_PREV \
-#   -v CONDO='TRUE' \
-#   -v MAPPED='FALSE'\
-#   -v CONDITION="WHERE right(a.bbl::bigint::text, 4) LIKE '75%%'" \
-#   -f sql/qaqc_null.sql &
-
-# psql $BUILD_ENGINE \
-#   -v VERSION=$VERSION \
-#   -v VERSION_PREV=$VERSION_PREV \
-#   -v CONDO='TRUE' \
-#   -v MAPPED='TRUE'\
-#   -v CONDITION="WHERE right(a.bbl::bigint::text, 4) LIKE '75%%' AND a.geom IS NOT NULL" \
-#   -f sql/qaqc_null.sql &
-
-# psql $BUILD_ENGINE \
-#   -v VERSION=$VERSION \
-#   -v VERSION_PREV=$VERSION_PREV \
-#   -v CONDO='FALSE' \
-#   -v MAPPED='FALSE'\
-#   -v CONDITION="" \
-#   -f sql/qaqc_null.sql &
-
-# psql $BUILD_ENGINE \
-#   -v VERSION=$VERSION \
-#   -v VERSION_PREV=$VERSION_PREV \
-#   -v CONDO='FALSE' \
-#   -v MAPPED='TRUE'\
-#   -v CONDITION="WHERE a.geom IS NOT NULL" \
-#   -f sql/qaqc_null.sql &
-
-# # QAQC AGGREGATE ANALYSIS
-# psql $BUILD_ENGINE \
-#   -v VERSION=$VERSION \
-#   -v CONDO='TRUE' \
-#   -v MAPPED='FALSE'\
-#   -v CONDITION="WHERE right(bbl::bigint::text, 4) LIKE '75%%'" \
-#   -f sql/qaqc_aggregate.sql &
-
-# psql $BUILD_ENGINE \
-#   -v VERSION=$VERSION \
-#   -v CONDO='TRUE' \
-#   -v MAPPED='TRUE'\
-#   -v CONDITION="WHERE right(bbl::bigint::text, 4) LIKE '75%%' AND geom IS NOT NULL" \
-#   -f sql/qaqc_aggregate.sql &
-
-# psql $BUILD_ENGINE \
-#   -v VERSION=$VERSION \
-#   -v CONDO='FALSE' \
-#   -v MAPPED='FALSE'\
-#   -v CONDITION="" \
-#   -f sql/qaqc_aggregate.sql &
-
-# psql $BUILD_ENGINE \
-#   -v VERSION=$VERSION \
-#   -v CONDO='FALSE' \
-#   -v MAPPED='TRUE'\
-#   -v CONDITION="WHERE geom IS NOT NULL" \
-#   -f sql/qaqc_aggregate.sql
-
-# wait 
-# echo 'QAQC is complete!'
-# exit 0
+for file in sql/qaqc_mismatch.sql sql/qaqc_aggregate.sql #sql/qaqc_null.sql
+do
+  for mapped in true false
+  do
+    for condo in true false 
+    do
+      QAQC $file $mapped $condo
+    done
+  done 
+done 
