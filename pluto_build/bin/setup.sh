832faf46d6f50d71e57a48723b336c79d1f26662
Andrew Brieff <abreff@gmail.com>
2022-07-06 13:04:13 -0400

    add back register

--- pluto_build/bin/setup.sh
+++ pluto_build/bin/setup.sh
@@ -3,3 +3,5 @@
 function setup {
     ./mc config host add spaces $AWS_S3_ENDPOINT $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY --api S3v4
 }
+
+register 'setup' 'init' 'install all dependencies' setup
