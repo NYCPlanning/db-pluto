02e6445afb24837307b7b1cea08034fa357f291d
Andrew Brieff <abreff@gmail.com>
2022-07-06 13:14:49 -0400

    add back register

--- pluto_build/bin/setup.sh
+++ pluto_build/bin/setup.sh
@@ -3,7 +3,7 @@
 function setup {
     curl https://dl.min.io/client/mc/release/linux-amd64/mc \
     --create-dirs \
-    -o $HOME/minio-binaries/mc \
+    -o $HOME/minio-binaries/mc 
     chmod +x $HOME/minio-binaries/mc 
     export PATH=$PATH:$HOME/minio-binaries
 
