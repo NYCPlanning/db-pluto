a2bf96ec7a9cb5d4b8665fec15c673faa9a0e6e2
Jingyi Huang <jh5721@nyu.edu>
2022-07-15 16:33:42 -0400

    Add new qaqc_outlier.sql for report generation

--- pluto_build/05_qaqc.sh
+++ pluto_build/05_qaqc.sh
@@ -42,7 +42,9 @@ function QAQC {
   -v MAPPED=$mapped  -v CONDITION="$condition" -f $file
 }
 
-
+psql $BUILD_ENGINE \
+  -v VERSION=$VERSION -v VERSION_PREV=$VERSION_PREV \
+  -f sql/qaqc_outlier.sql 
 
 # QAQC MISMATCH ANALYSIS
 for file in sql/qaqc_aggregate.sql sql/qaqc_mismatch.sql sql/qaqc_null.sql
