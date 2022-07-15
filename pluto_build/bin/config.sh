31d71301946410d3ed7fa0eb04ca1a5914ed1f2c
Sasha Weinstein <70385853+SashaWeinstein@users.noreply.github.com>
2022-07-15 10:29:14 -0400

    restore echo of url

--- pluto_build/bin/config.sh
+++ pluto_build/bin/config.sh
@@ -146,6 +146,7 @@ function import_qaqc {
     echo "✅ $name.sql exists in cache"
   else
     echo "🛠 $name.sql doesn't exists in cache, downloading ..."
+    echo "{$url}/{$name.sql}"
     mkdir -p $target_dir && (
       cd $target_dir
       curl -ss -O $qaqc_do_url/$name.sql
