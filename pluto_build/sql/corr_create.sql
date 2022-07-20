708ba35f41c5649b949860cf976c800d8b266bb8
Andrew Brieff <abreff@gmail.com>
2022-07-20 10:52:59 -0400

    copy schema from pluto corrections

--- pluto_build/sql/corr_create.sql
+++ pluto_build/sql/corr_create.sql
@@ -3,23 +3,7 @@ ALTER TABLE pluto
 ADD COLUMN dcpedited text;
 
 DROP TABLE IF EXISTS pluto_corrections_not_applied;
-CREATE TABLE pluto_corrections_not_applied (
-	bbl 		    VARCHAR,
-	field 	  		VARCHAR,
-	old_value 		VARCHAR,
-	new_value 		VARCHAR,
-	type            VARCHAR,
-    reason          VARCHAR,
-    version         VARCHAR
-);
+CREATE TABLE pluto_corrections_not_applied (LIKE pluto_corrections INCLUDING ALL);
 
 DROP TABLE IF EXISTS pluto_corrections_applied;
-CREATE TABLE pluto_corrections_applied (
-	bbl 		    VARCHAR,
-	field 	  		VARCHAR,
-	old_value 		VARCHAR,
-	new_value 		VARCHAR,
-	type            VARCHAR,
-    reason          VARCHAR,
-    version         VARCHAR
-);
+CREATE TABLE pluto_corrections_applied (LIKE pluto_corrections INCLUDING ALL);
