f517d8ce2d9628933a12fe7b96ff2b6a20f1dfa8
Te Du <tedudevelop@gmail.com>
2022-07-22 10:13:55 -0400

    gdal bin

--- pluto_build/bin/setup.sh
+++ pluto_build/bin/setup.sh
@@ -8,6 +8,6 @@ curl -O https://dl.min.io/client/mc/release/linux-amd64/mc
 chmod +x mc
 sudo mv ./mc /usr/bin
 mc config host add spaces $AWS_S3_ENDPOINT $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY --api S3v4
-sudo apt-get -y install --no-install-recommends gdal
+sudo apt-get -y install --no-install-recommends gdal-bin
 }
 register 'setup' 'init' 'install all dependencies' setup
