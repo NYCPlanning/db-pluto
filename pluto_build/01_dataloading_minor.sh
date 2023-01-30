51a78624bed7933dab36ad2bf4ed98e4d0dd6460
mbh329 <mbrueckner@planning.nyc.gov>
2023-01-30 16:14:27 -0500

    update data sources to match 22v3 pluto release

--- pluto_build/01_dataloading_minor.sh
+++ pluto_build/01_dataloading_minor.sh
@@ -14,14 +14,17 @@ if [[ $1 == "drop" ]]; then
     "
 fi
 
+# This shell script is for minor releases of PLUTO where all datasets remain constant except  
+# for the Zoning related data which updated monthly
+
 # import_public PTS and CAMA from data library
-import_public pluto_pts 20230130 &  
-import_public pluto_input_cama_dof 20221130 & #this process is broken otherwise all pluto datasets should be same date
-import_public pluto_input_numbldgs 20230130 &
-import_public pluto_input_geocodes 20230130 &
+import_public pluto_pts $PLUTO_WEEKLY_DATA_VERSION &  
+import_public pluto_input_cama_dof 20220927 & 
+import_public pluto_input_numbldgs $PLUTO_WEEKLY_DATA_VERSION &
+import_public pluto_input_geocodes $PLUTO_WEEKLY_DATA_VERSION &
 
 # import_public spatial bounaries from data library
-import_public dcp_edesignation & #scheduled update?
+import_public dcp_edesignation 20221109 & 
 import_public dcp_cdboundaries_wi $GEOSUPPORT_VERSION &
 import_public dcp_cb2010_wi $GEOSUPPORT_VERSION &
 import_public dcp_ct2010_wi $GEOSUPPORT_VERSION &
@@ -33,8 +36,8 @@ import_public dcp_policeprecincts $GEOSUPPORT_VERSION &
 import_public dcp_councildistricts_wi $GEOSUPPORT_VERSION &
 import_public dcp_healthareas $GEOSUPPORT_VERSION &
 import_public dcp_healthcenters $GEOSUPPORT_VERSION &
-import_public dof_shoreline & #scheduled update?
-import_public doitt_zipcodeboundaries 20210405 & 
+import_public dof_shoreline $DOF_DATA_VERSION & 
+import_public doitt_zipcodeboundaries 20180910 & 
 import_public fema_firms2007_100yr $FEMA_FIRPS_VERSION &
 import_public fema_pfirms2015_100yr $FEMA_FIRPS_VERSION &
 
@@ -48,14 +51,14 @@ import_public dcp_zoningmapamendments &
 import_public dcp_zoningmapindex &
 
 # import_public other
-import_public pluto_corrections $GEOSUPPORT_VERSION &
-import_public dpr_greenthumb 20230124 &
-import_public dsny_frequencies 20230128 &
+import_public pluto_corrections &
+import_public dpr_greenthumb 20221101 &
+import_public dsny_frequencies 20221105 &
 import_public lpc_historic_districts 20220526 &
 import_public lpc_landmarks 20220613 &
 import_public dcp_colp 202206 &
-import_public dof_dtm $DOF_VERSION &
-import_public dof_condo $DOF_VERSION
+import_public dof_dtm $DOF_DATA_VERSION &
+import_public dof_condo $DOF_DATA_VERSION
 
 wait
 
