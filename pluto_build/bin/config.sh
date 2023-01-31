ea200350407ecd154530098652f12044dbb18fd9
Damon McCullough <dmccullough@planning.nyc.gov>
2023-01-31 13:43:04 -0500

    remove bash traps

--- pluto_build/bin/config.sh
+++ pluto_build/bin/config.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Exit when any command fails
-set -e
-# keep track of the last executed command
-trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
-# echo an error message before exiting
-trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
-
 function set_env {
   for envfile in $@
   do
