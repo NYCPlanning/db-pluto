c0901aceaccd0a8690b0a011d732f7c9e0af2910
Damon McCullough <dmccullough@planning.nyc.gov>
2023-02-01 11:17:42 -0500

    move all versions to env file, rename, and reorder

--- pluto_build/01_dataloading_minor.sh
+++ pluto_build/01_dataloading_minor.sh
@@ -15,10 +15,10 @@ if [[ $1 == "drop" ]]; then
 fi
 
 # This shell script is for minor releases of PLUTO where all datasets remain constant
-# except for DCP data that is updated monthly (e.g. E-Designations, Zoning)
+# except for DCP data that is updated monthly and used by ZOLA (e.g. E-Designations, zoning files)
 
-# import_public zoning files from data library - default to latest
-import_public dcp_edesignation & 
+# import zoning files from data library - default to latest
+import_public dcp_edesignation &
 import_public dcp_commercialoverlay &
 import_public dcp_limitedheight &
 import_public dcp_zoningdistricts &
@@ -27,13 +27,13 @@ import_public dcp_specialpurposesubdistricts &
 import_public dcp_zoningmapamendments &
 import_public dcp_zoningmapindex &
 
-# import_public PTS and CAMA from data library
-import_public pluto_pts $PLUTO_WEEKLY_DATA_VERSION &  
-import_public pluto_input_cama_dof 20220927 & 
-import_public pluto_input_numbldgs $PLUTO_WEEKLY_DATA_VERSION &
-import_public pluto_input_geocodes $PLUTO_WEEKLY_DATA_VERSION &
+# import PTS and CAMA from data library
+import_public pluto_input_numbldgs $DOF_WEEKLY_DATA_VERSION &
+import_public pluto_input_geocodes $DOF_WEEKLY_DATA_VERSION &
+import_public pluto_pts $DOF_WEEKLY_DATA_VERSION &
+import_public pluto_input_cama_dof $DOF_CAMA_DATA_VERSION &
 
-# import_public spatial bounaries from data library
+# import spatial bounaries from data library
 import_public dcp_cdboundaries_wi $GEOSUPPORT_VERSION &
 import_public dcp_cb2010_wi $GEOSUPPORT_VERSION &
 import_public dcp_ct2010_wi $GEOSUPPORT_VERSION &
@@ -45,20 +45,22 @@ import_public dcp_policeprecincts $GEOSUPPORT_VERSION &
 import_public dcp_councildistricts_wi $GEOSUPPORT_VERSION &
 import_public dcp_healthareas $GEOSUPPORT_VERSION &
 import_public dcp_healthcenters $GEOSUPPORT_VERSION &
-import_public dof_shoreline $DOF_DATA_VERSION & 
-import_public doitt_zipcodeboundaries 20180910 & 
 import_public fema_firms2007_100yr $FEMA_FIRPS_VERSION &
 import_public fema_pfirms2015_100yr $FEMA_FIRPS_VERSION &
+import_public doitt_zipcodeboundaries $DOITT_DATA_VERSION & 
+import_public dof_shoreline $DOF_DATA_VERSION & 
 
-# import_public other
-import_public pluto_corrections 21v4 &
-import_public dpr_greenthumb 20221101 &
-import_public dsny_frequencies 20221105 &
-import_public lpc_historic_districts 20220526 &
-import_public lpc_landmarks 20220613 &
-import_public dcp_colp 202206 &
+# import other
 import_public dof_dtm $DOF_DATA_VERSION &
-import_public dof_condo $DOF_DATA_VERSION
+import_public dof_condo $DOF_DATA_VERSION &
+import_public dcp_colp $DCP_COLP_VERSION &
+import_public dpr_greenthumb $DPR_GREENTHUMB_VERSION &
+import_public dsny_frequencies $DSNY_FREQUENCIES_VERSION &
+import_public lpc_historic_districts $LPC_HISTORIC_DISTRICTS_VERSION &
+import_public lpc_landmarks $LPC_LANDMARKS_VESRSION
+
+# import pluto corrections
+import_public pluto_corrections $PLUTO_CORRECTIONS_VERSION &
 
 wait
 
