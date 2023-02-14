8b972452173d8b3bf639bfe5ecfa17ba81cf88c6
mbh329 <mbrueckner@planning.nyc.gov>
2023-02-14 11:37:44 -0500

    clean up comments

--- pluto_build/sql/qaqc_housing_units.sql
+++ pluto_build/sql/qaqc_housing_units.sql
@@ -1,16 +1,15 @@
--- Create qaqc table All PLUTO records where there is a match in Housing Database 
+-- Create qaqc table of all PLUTO records where there is a match in Housing Database 
 -- and the PLUTO residential units value does not match the housing database certificates 
 -- of occupancy value. Have flag indicating if bbl has residential unit correction in manual corrections table.
--- select PLUTO records that have a match in the HousingDB subset where unitsres does not equal units co
 
 
 -- Data Dictionary 
 -- bbl - billing BBL
 -- job_number - The DOB job application number assigned when the applicant begins the application. This is the unique identifier for the application submitted to the Department of Buildings (DOB). 
--- units_res - The number of residential units as reported by dcp_housing database
+-- unitsres - The number of residential units as reported by PLUTO database
 -- units_co - The number of units listed on the DOB issued Certificate of Occupancy 
--- new_value 
--- old_value 
+-- new_value - The new number of residential units as reported by the current version of PLUTO
+-- old_value - The old number of residential units as reported by the previous version of PLUTO
 
 
 CREATE TABLE IF NOT EXISTS qaqc_housing_units(
